98118468ec67faf11cba53407186858fec1f5ca1
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-10 17:21:57 -0500

    Fixing registration and login pages

--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -1,21 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import CSSModules from "react-css-modules";
+import styles from "../styles/Auth.module.css";
+// import invisible from "../public/svgs/invisible.svg";
+// import visible from "../public/svgs/visible.svg";
 
 const Login = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
-  const handleSubmit = () => {
+  const handleSubmit = () => {};
 
-    console.log("Bob Jones Locos Pollos")
+  const handlePasswordVisiblity = () => {
+    setPasswordVisible(prevVisibility => !prevVisibility);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Bob Jones</label>
-      <input type="email" id="email" name="email" placeholder="you@example.com" required/>
-      <label htmlFor="password">Bob Jones</label>
-      <input type="password" id="password" name="password" placeholder="1234" required/>
-      <button type="submit">Log In</button>
-    </form>
+    <section styleName="auth">
+      <h2 styleName="auth__subtitle">Login</h2>
+      <form onSubmit={handleSubmit} styleName="auth__form">
+        <div styleName="auth__control">
+          <label htmlFor="email" styleName="auth__label">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="you@example.com"
+            required
+            styleName="auth__input"
+          />
+        </div>
+        <div styleName="auth__control">
+          <label htmlFor="password" styleName="auth__label">
+            Password
+          </label>
+          <div styleName="auth__flex">
+            <input
+              type={passwordVisible ? "text" : "password"}
+              id="password"
+              name="password"
+              placeholder="1234"
+              required
+              styleName="auth__input"
+            />
+            {passwordVisible ? (
+              <div styleName="auth__container" onClick={handlePasswordVisiblity}>
+                <img
+                  src="/svgs/invisible.svg"
+                  alt="crossed out eye"
+                  styleName="auth__icon"
+                />
+              </div>
+            ) : (
+              <div styleName="auth__container" onClick={handlePasswordVisiblity}>
+                <img
+                  src="/svgs/visible.svg"
+                  alt="open eye"
+                  styleName="auth__icon"
+                />
+              </div>
+            )}
+          </div>
+        </div>
+        <button type="submit" styleName="auth__button">
+          Log In
+        </button>
+      </form>
+    </section>
   );
 };
 
-export default Login;
+export default CSSModules(Login, styles, {
+  allowMultiple: true,
+  handleNotFoundStyleName: "log",
+});
--- web/pages/register.tsx
+++ web/pages/register.tsx
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import CSSModules from "react-css-modules";
 import styles from "../styles/Auth.module.css";
 
 const Register = () => {
-  const handleSubmit = () => {};
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const handleSubmit = () => {
+    console.log("Bob Jones Locos Pollos");
+  };
+
+  const handlePasswordVisiblity = () => {
+    setPasswordVisible((prevVisibility) => !prevVisibility);
+  };
 
   return (
     <section styleName="auth">
-      <h1 styleName="auth__title">Optimistic Facebook</h1>
-      <h2 styleName="auth__subtitle">Think again asshole</h2>
+      <h2 styleName="auth__subtitle">Register</h2>
       <form onSubmit={handleSubmit} styleName="auth__form">
         <div styleName="auth__control">
           <label htmlFor="email" styleName="auth__label">
-            Bob Jones
+            First Name
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="you@example.com"
+            required
+            styleName="auth__input"
+          />
+        </div>
+        <div styleName="auth__control">
+          <label htmlFor="email" styleName="auth__label">
+            Last Name
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="you@example.com"
+            required
+            styleName="auth__input"
+          />
+        </div>
+        <div styleName="auth__control">
+          <label htmlFor="email" styleName="auth__label">
+            Email
           </label>
           <input
             type="email"
@@ -25,27 +58,51 @@ const Register = () => {
         </div>
         <div styleName="auth__control">
           <label htmlFor="password" styleName="auth__label">
-            Bob Jones
+            Password
           </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="1234"
-            required
-            styleName="auth__input"
-          />
+          <div styleName="auth__flex">
+            <input
+              type={passwordVisible ? "text" : "password"}
+              id="password"
+              name="password"
+              placeholder="1234"
+              required
+              styleName="auth__input"
+            />
+            {passwordVisible ? (
+              <div
+                styleName="auth__container"
+                onClick={handlePasswordVisiblity}
+              >
+                <img
+                  src="/svgs/invisible.svg"
+                  alt="crossed out eye"
+                  styleName="auth__icon"
+                />
+              </div>
+            ) : (
+              <div
+                styleName="auth__container"
+                onClick={handlePasswordVisiblity}
+              >
+                <img
+                  src="/svgs/visible.svg"
+                  alt="open eye"
+                  styleName="auth__icon"
+                />
+              </div>
+            )}
+          </div>
         </div>
         <button type="submit" styleName="auth__button">
-          Log In
+          Register
         </button>
       </form>
-      <hr />
     </section>
   );
 };
 
 export default CSSModules(Register, styles, {
   allowMultiple: true,
-  handleNotFoundStyleName: "log",
+  handleNotFoundStyleName: "ignore",
 });
