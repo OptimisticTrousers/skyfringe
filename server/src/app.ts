22e1b262e6e99f6ec0fe8791d21c8af185875f8b
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-10 22:46:07 -0500

    Installing packages to be used on the server relating to JWT and authentication

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,51 +1,32 @@
-import express, {
-  Application,
-  ErrorRequestHandler,
-  NextFunction,
-  Response,
-  Request,
-} from "express";
+import express, { Application } from "express";
 import path from "path";
 import logger from "morgan";
-import createError from "http-errors";
 import cors from "cors";
 import { config } from "dotenv";
-import mongoose, { ConnectOptions, Error } from "mongoose";
 import routes from "./routes/index";
+import mongoConfig from "./config/db";
+import passportConfig from "./config/passport";
+import notFoundHandler from "./middleware/notFoundHandler";
+import errorHandler from "./middleware/errorHandler";
 
 config();
 
 const app: Application = express();
 
-const mongoDB = process.env.DB_STRING;
-mongoose.connect(mongoDB!, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-} as ConnectOptions);
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "mongo connection error"));
-
+mongoConfig();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
+passportConfig();
 
+// routes
 app.use("/api", routes);
 
 // catch 404 and forward to error handler
-app.use((req: Request, res: Response, next: NextFunction) => {
-  next(createError(404));
-});
-
+app.use(notFoundHandler);
 // error handler
-app.use(<ErrorRequestHandler>((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(errorHandler);
 
-  // send back an error
-  res.status(err.status || 500).send(err);
-}));
-
-app.listen(5000, () => console.log("Server running"));
+app.listen(process.env.PORT, () => console.log("Server running"));
