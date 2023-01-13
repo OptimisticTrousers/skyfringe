2bb63768308fcdcaa4ca9c3d9539716e44be8e68
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-13 17:45:39 -0500

    Creating a top menu component for responsiveness

---
+++ web/components/TopMenu/TopMenu.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import CSSModules from "react-css-modules";
+import styles from "./TopMenu.module.css";
+import { MdOutlineRecommend } from "react-icons/md";
+import Suggestions from "../Suggestions/Suggestions";
+import Recent from "../Recent/Recent";
+// import {MdRecentActors} from "react-icons/md;"
+
+const TopMenu = () => {
+  const [toggleSuggestions, setToggleSuggestions] = useState(false);
+
+  const [toggleActivities, setToggleActivities] = useState(false);
+
+  const handleSuggestions = () => {
+    setToggleSuggestions((prevValue) => !prevValue);
+  };
+
+  const handleActivities = () => {
+    setToggleActivities((prevValue) => !prevValue);
+  };
+  return (
+    <header styleName="menu">
+      <div styleName="menu__buttons">
+        <button styleName="menu__button" onClick={handleSuggestions}>
+          Suggestions For you
+        </button>
+        <button styleName="menu__button" onClick={handleActivities}>
+          Recent Activity
+        </button>
+      </div>
+      <div styleName={`menu__action ${toggleSuggestions && "menu__action--active"}`}>{toggleSuggestions && <Suggestions />}</div>
+      <div styleName={`menu__action ${toggleActivities && "menu__action--active"}`}>{toggleActivities && <Recent />}</div>
+    </header>
+  );
+};
+
+export default CSSModules(TopMenu, styles, {
+  allowMultiple: true,
+  handleNotFoundStyleName: "ignore",
+});
