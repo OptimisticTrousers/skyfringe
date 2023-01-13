20f0bade6dbbd62a3114513f4cb7810594e58821
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-13 13:05:36 -0500

    Finishing aside

--- web/components/Aside/Aside.tsx
+++ web/components/Aside/Aside.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CSSModules from "react-css-modules";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillCalendarDateFill } from "react-icons/bs";
@@ -6,49 +6,64 @@ import { FaShoppingBag } from "react-icons/fa";
 import { IoIosPeople } from "react-icons/io";
 import { RiNewspaperFill } from "react-icons/ri";
 import styles from "./Aside.module.css";
+import { GrFormClose } from "react-icons/gr";
+import { RiSettings3Line } from "react-icons/ri";
+import {AiFillCloseSquare} from "react-icons/ai";
 
 const Aside = () => {
+
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+
+  const toggle = () => {
+    setToggleSidebar(prevToggleValue => !prevToggleValue);
+  }
   return (
-    <aside styleName="home__aside">
-      <div styleName="home__logo">
-        <img src="optimistictrousers.jpg" styleName="home__image" />
-        <h1>OptimisticTrousers</h1>
+    <>
+      <div styleName="hamburger" onClick={toggle}>
+        <div styleName="hamburger__line"></div>
+        <div styleName="hamburger__line"></div>
+        <div styleName="hamburger__line"></div>
       </div>
-      <div styleName="home__wrapper">
-        {/* <div styleName="home__icon-container">
-              <RiSearchLine />
-            </div> */}
-        <input styleName="home__input" placeholder="Explore kaloka..." />
-      </div>
-      <div styleName="home__links">
-        <a styleName="home__link">
-          <AiFillHome />
-          <p styleName="home__text">Home</p>
-        </a>
-        <a styleName="home__link">
-          <IoIosPeople />
-          <p styleName="home__text">Community</p>
-        </a>
-        <a styleName="home__link">
-          <FaShoppingBag />
-          <p styleName="home__text">Marketplace</p>
-        </a>
-        <a styleName="home__link">
-          <BsFillCalendarDateFill />
-          <p styleName="home__text">Kaloka events</p>
-        </a>
-        <a styleName="home__link">
-          <RiNewspaperFill />
-          <p styleName="home__text">News feed</p>
-        </a>
-      </div>
-      <div styleName="home__communities">
-        <div styleName="home__header">
-          <h2 styleName="home__subtitle">My community</h2>
-          <p styleName="home__number">29</p>
+      <aside styleName={`aside ${toggleSidebar && "aside--open"}`}>
+        <div styleName="aside__container">
+          <img
+            src="https://mmackz.github.io/admin-dashboard/images/logoipsum-logo-54.svg"
+            alt="logo"
+          />
+          <AiFillCloseSquare styleName="aside__icon aside__icon--exit" onClick={toggle}/>
         </div>
-      </div>
-    </aside>
+        <nav styleName="aside__nav">
+          <ul styleName="aside__list">
+            <li styleName="aside__item">
+              <AiFillHome styleName="aside__icon" />
+              <p styleName="aside__name">Home</p>
+            </li>
+            <li styleName="aside__item">
+              <IoIosPeople styleName="aside__icon" />
+              <p styleName="aside__name">Community</p>
+            </li>
+            <li styleName="aside__item">
+              <FaShoppingBag styleName="aside__icon" />
+              <p styleName="aside__name">Marketplace</p>
+            </li>
+            <li styleName="aside__item">
+              <BsFillCalendarDateFill styleName="aside__icon" />
+              <p styleName="aside__name">Kaloka events</p>
+            </li>
+            <li styleName="aside__item">
+              <RiNewspaperFill styleName="aside__icon" />
+              <p styleName="aside__name">News feed</p>
+            </li>
+          </ul>
+          <ul styleName="aside__list">
+            <li styleName="aside__item">
+              <RiSettings3Line styleName="aside__icon" />
+              <p styleName="aside__name">Settings</p>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
   );
 };
 
