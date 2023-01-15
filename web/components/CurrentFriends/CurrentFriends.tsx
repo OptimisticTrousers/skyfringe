2de2aaaa4c7133dca2e25e0647ed9f0c1a45610c
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-14 21:25:39 -0500

    Implementing friends page

---
+++ web/components/CurrentFriends/CurrentFriends.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import CSSModules from "react-css-modules";
+import Card from "../Card/Card";
+import styles from "../Friends/Friends.module.css";
+
+const CurrentFriends = () => {
+  return (
+    <div styleName="friend">
+      <Card>
+        <h2 styleName="friend__title">Current Friends</h2>
+        <div styleName="friend__flex">
+          <div styleName="friend__details">
+            <img src="optimistictrousers.jpg" styleName="friend__avatar" />
+            <p styleName="friend__name">Bob Jones</p>
+          </div>
+          <button styleName="friend__button">Remove</button>
+        </div>
+        <div styleName="friend__flex">
+          <div styleName="friend__details">
+            <img src="optimistictrousers.jpg" styleName="friend__avatar" />
+            <p styleName="friend__name">Bob Jones</p>
+          </div>
+          <button styleName="friend__button">Remove</button>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default CSSModules(CurrentFriends, styles, {
+  allowMultiple: true,
+  handleNotFoundStyleName: "ignore",
+});
---
+++ web/components/FriendRequestsReceived/FriendRequestsReceived.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import CSSModules from "react-css-modules";
+import Card from "../Card/Card";
+import styles from "../Friends/Friends.module.css";
+
+const FriendRequestsReceived = () => {
+  return (
+    <div styleName="friend">
+      <Card>
+        <h2 styleName="friend__title">Friend Requests Received</h2>
+        <div styleName="friend__flex">
+          <div styleName="friend__details">
+            <img src="optimistictrousers.jpg" styleName="friend__avatar" />
+            <p styleName="friend__name">Bob Jones</p>
+          </div>
+          <button styleName="friend__button">Accept</button>
+        </div>
+        <div styleName="friend__flex">
+          <div styleName="friend__details">
+            <img src="optimistictrousers.jpg" styleName="friend__avatar" />
+            <p styleName="friend__name">Bob Jones</p>
+          </div>
+          <button styleName="friend__button">Accept</button>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default CSSModules(FriendRequestsReceived, styles, {
+  allowMultiple: true,
+  handleNotFoundStyleName: "ignore",
+});
