ff3e7567472aad7cac5e94831e28e3df89ccd190
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-16 17:29:12 -0500

    FIxing 'TopMenu' component

--- web/components/TopMenu/TopMenu.tsx
+++ web/components/TopMenu/TopMenu.tsx
@@ -2,28 +2,19 @@ import React, { useState } from "react";
 import CSSModules from "react-css-modules";
 import styles from "./TopMenu.module.css";
 import Suggestions from "../Suggestions/Suggestions";
-import Recent from "../Recent/Recent";
 
 const TopMenu = () => {
   const [toggleSuggestions, setToggleSuggestions] = useState(false);
-
-  const [toggleActivities, setToggleActivities] = useState(false);
 
   const handleSuggestions = () => {
     setToggleSuggestions((prevValue) => !prevValue);
   };
 
-  const handleActivities = () => {
-    setToggleActivities((prevValue) => !prevValue);
-  };
   return (
     <header styleName="menu">
       <div styleName="menu__buttons">
         <button styleName="menu__button" onClick={handleSuggestions}>
           Suggestions For you
-        </button>
-        <button styleName="menu__button" onClick={handleActivities}>
-          Recent Activity
         </button>
       </div>
       <div
@@ -32,11 +23,6 @@ const TopMenu = () => {
         }`}
       >
         {toggleSuggestions && <Suggestions />}
-      </div>
-      <div
-        styleName={`menu__action ${toggleActivities && "menu__action--active"}`}
-      >
-        {toggleActivities && <Recent />}
       </div>
     </header>
   );
