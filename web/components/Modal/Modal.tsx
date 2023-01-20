eb19c8dcdc78458fd442393f7f967fe065774eb4
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-20 16:30:18 -0500

    Finishing out 'Modal' component

--- web/components/Modal/Modal.tsx
+++ web/components/Modal/Modal.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from "react"
-import CSSModules from "react-css-modules"
+import React, { FC } from "react";
+import CSSModules from "react-css-modules";
 import { AiFillCloseSquare } from "react-icons/ai";
 import styles from "./Modal.module.css";
+import {FcGallery} from "react-icons/fc";
 
 interface Props {
   title: string;
@@ -11,15 +12,43 @@ const Modal = () => {
   return (
     <div styleName="modal">
       <form styleName="modal__form">
-        <h2 styleName="modal__title">Create post</h2>
-        {/* <button styleName="modal__button modal__button--exit"></button> */}
-        <AiFillCloseSquare styleName="modal__icon"/>
+        <button styleName="modal__button modal__button--exit">
+          <AiFillCloseSquare styleName="modal__icon" />
+        </button>
+        <div styleName="modal__title-bar">
+          <h2 styleName="modal__title">Create post</h2>
+        </div>
+        <div styleName="modal__author-bar">
+          <img src="/images/optimistictrousers.jpg" styleName="modal__avatar" />
+          <div styleName="modal__text">
+            <p styleName="modal__surtitle">posting as</p>
+            <h3 styleName="modal__name">Bob Jones</h3>
+          </div>
+        </div>
+        <div styleName="modal__content">
+          <textarea styleName="modal__textarea" placeholder="What's cooking, Bob?"></textarea>
+        </div>
+        <div styleName="modal__controls">
+          <div styleName="modal__interactives">
+            <div styleName="modal__emoji">
+              🙂
+            </div>
+            <label styleName="modal__label">
+              <input type="file"styleName="modal__file" />
+              <FcGallery styleName="modal__icon modal__icon--gallery" />
+              Add picture
+            </label>
+          </div>
+          <button styleName="modal__button modal__button--submit">
+            Post
+          </button>
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default CSSModules(Modal, styles, {
   allowMultiple: true,
-  handleNotFoundStyleName: "ignore"
-})
+  handleNotFoundStyleName: "ignore",
+});
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,24 +6,28 @@ import Suggestions from "../components/Suggestions/Suggestions";
 import HomeFriends from "../components/HomeFriends/HomeFriends";
 import TopMenu from "../components/TopMenu/TopMenu";
 import SideFooter from "../components/SideFooter/SideFooter";
+import Modal from "../components/Modal/Modal";
 
 function Home() {
+
   return (
-    <div styleName="home">
-      <div styleName="home__center">
-        <div styleName="home__menu">
-          <TopMenu />
+    <>
+      <div styleName="home">
+        <div styleName="home__center">
+          <div styleName="home__menu">
+            <TopMenu />
+          </div>
+          <HomeFriends />
+          <CreatePost />
+          <Post />
         </div>
-        <HomeFriends />
-        <CreatePost />
-        <Post />
+        <aside styleName="home__right">
+          <Suggestions />
+          {/* <Recent /> */}
+          <SideFooter />
+        </aside>
       </div>
-      <aside styleName="home__right">
-        <Suggestions />
-        {/* <Recent /> */}
-        <SideFooter />
-      </aside>
-    </div>
+    </>
   );
 }
 
