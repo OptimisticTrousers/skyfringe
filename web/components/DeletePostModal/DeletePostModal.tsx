b7de0c9dbdd857f2ac0dadbe0dc07596ad06c6cc
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-24 14:20:43 -0500

    Creating 'DeleteAccountModal' on settings page

---
+++ web/components/DeletePostModal/DeletePostModal.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+import CSSModules from "react-css-modules";
+import styles from "./DeletePostModal.module.css";
+import ModalContainer from "../ModalContainer/ModalContainer";
+
+interface Props {
+  toggleModal: () => void;
+}
+
+const DeletePostModal: FC<Props> = ({ toggleModal }) => {
+  return (
+    <ModalContainer title="Delete Account" toggleModal={toggleModal}>
+      <form styleName="modal">
+        <p styleName="modal__alert">
+          Are you sure you want to delete your account?
+        </p>
+        <p styleName="modal__message">
+          Once your account has been deleted, it cannot be recovered. You cannot
+          undo this action.
+        </p>
+        <button styleName="modal__button">Delete Account</button>
+      </form>
+    </ModalContainer>
+  );
+};
+
+export default CSSModules(DeletePostModal, styles, {
+  allowMultiple: true,
+  handleNotFoundStyleName: "ignore",
+});
--- web/pages/settings.tsx
+++ web/pages/settings.tsx
@@ -1,105 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import CSSModules from "react-css-modules";
 import Card from "../components/Card/Card";
 import styles from "../styles/Settings.module.css";
-import {AiFillCamera} from "react-icons/ai";
-import {AiOutlineCamera} from "react-icons/ai";
+import { AiFillCamera } from "react-icons/ai";
+import { AiOutlineCamera } from "react-icons/ai";
+import DeletePostModal from "../components/DeletePostModal/DeletePostModal";
 
 const Settings = () => {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsDeleteModalOpen((prevValue) => !prevValue);
+  };
+
   return (
-    <div styleName="settings">
-      <header styleName="settings__header">
-        <button styleName="settings__button settings__button--banner">
-          <AiOutlineCamera styleName="settings__icon" />
-          {/* <AiFillCamera styleName="settings__icon" /> */}
-          Change Cover
-        </button>
-        <img
-          src="/images/optimistictrousers.jpg"
-          styleName="settings__banner"
-        />
-      </header>
-      <div styleName="settings__grid">
-        <aside styleName="settings__aside">
-          <Card>
-            <div styleName="settings__card">
-              <img
-                src="/images/optimistictrousers.jpg"
-                styleName="settings__avatar"
-              />
-              <h2 styleName="settings__name">Nathaniel Poole</h2>
-              <p styleName="settings__friends">10 friends</p>
-            </div>
-            <div styleName="settings__box">
-              <p styleName="settings__statistic">Posts created</p>
-              <p styleName="settings__number settings__number--orange">32</p>
-            </div>
-            <div styleName="settings__box">
-              <p styleName="settings__statistic">Posts liked</p>
-              <p styleName="settings__number settings__number--green">32</p>
-            </div>
-            <div styleName="settings__box">
-              <p styleName="settings__statistic">Comments</p>
-              <p styleName="settings__number settings__number--gray">32</p>
-            </div>
-            <div styleName="settings__box">
-              <button styleName="settings__button settings__button--view">
-                View Public Profile
+    <>
+      <div styleName="settings">
+        <header styleName="settings__header">
+          <button styleName="settings__button settings__button--banner">
+            <AiOutlineCamera styleName="settings__icon" />
+            {/* <AiFillCamera styleName="settings__icon" /> */}
+            Change Cover
+          </button>
+          <img
+            src="/images/optimistictrousers.jpg"
+            styleName="settings__banner"
+          />
+        </header>
+        <div styleName="settings__grid">
+          <aside styleName="settings__aside">
+            <Card>
+              <div styleName="settings__card">
+                <img
+                  src="/images/optimistictrousers.jpg"
+                  styleName="settings__avatar"
+                />
+                <h2 styleName="settings__name">Nathaniel Poole</h2>
+                <p styleName="settings__friends">10 friends</p>
+              </div>
+              <div styleName="settings__box">
+                <p styleName="settings__statistic">Posts created</p>
+                <p styleName="settings__number settings__number--orange">32</p>
+              </div>
+              <div styleName="settings__box">
+                <p styleName="settings__statistic">Posts liked</p>
+                <p styleName="settings__number settings__number--green">32</p>
+              </div>
+              <div styleName="settings__box">
+                <p styleName="settings__statistic">Comments</p>
+                <p styleName="settings__number settings__number--gray">32</p>
+              </div>
+              <div styleName="settings__box">
+                <button styleName="settings__button settings__button--view">
+                  View Public Profile
+                </button>
+              </div>
+            </Card>
+            <Card>
+              <h3 styleName="settings__subtitle">Close account</h3>
+              <p styleName="settings__description">
+                By deleting your account, you will lose all of your data. This
+                action cannot be reversed.
+              </p>
+              <button
+                styleName="settings__button--delete"
+                onClick={toggleModal}
+              >
+                Close Account
               </button>
-            </div>
-          </Card>
-          <Card>
-            <h3 styleName="settings__subtitle">Close account</h3>
-            <p styleName="settings__description">
-              By deleting your account, you will lose all of your data. This
-              action cannot be reversed.
-            </p>
-            <button styleName="settings__button--delete">Close Account</button>
-          </Card>
-        </aside>
-        <div styleName="settings__content">
-          <Card>
-            <h2 styleName="settings__title">Account Settings</h2>
-            <form styleName="settings__form">
-              <div styleName="settings__group">
-                <label htmlFor="firstName" styleName="settings__label">
-                  First Name
-                </label>
-                <input
-                  styleName="settings__input"
-                  id="firstName"
-                  name="firstName"
-                />
+            </Card>
+          </aside>
+          <div styleName="settings__content">
+            <Card>
+              <h2 styleName="settings__title">Account Settings</h2>
+              <form styleName="settings__form">
+                <div styleName="settings__group">
+                  <label htmlFor="firstName" styleName="settings__label">
+                    First Name
+                  </label>
+                  <input
+                    styleName="settings__input"
+                    id="firstName"
+                    name="firstName"
+                  />
+                </div>
+                <div styleName="settings__group">
+                  <label htmlFor="lastName" styleName="settings__label">
+                    Last Name
+                  </label>
+                  <input
+                    styleName="settings__input"
+                    id="lastName"
+                    name="lastName"
+                  />
+                </div>
+                <div styleName="settings__group">
+                  <label htmlFor="email" styleName="settings__label">
+                    Email
+                  </label>
+                  <input
+                    styleName="settings__input"
+                    id="email"
+                    name="email"
+                    type="email"
+                  />
+                </div>
+              </form>
+              <div styleName="settings__box">
+                <button styleName="settings__button--update">Update</button>
               </div>
-              <div styleName="settings__group">
-                <label htmlFor="lastName" styleName="settings__label">
-                  Last Name
-                </label>
-                <input
-                  styleName="settings__input"
-                  id="lastName"
-                  name="lastName"
-                />
-              </div>
-              <div styleName="settings__group">
-                <label htmlFor="email" styleName="settings__label">
-                  Email
-                </label>
-                <input
-                  styleName="settings__input"
-                  id="email"
-                  name="email"
-                  type="email"
-                />
-              </div>
-            </form>
-            <div styleName="settings__box">
-              <button styleName="settings__button--update">Update</button>
-            </div>
-          </Card>
+            </Card>
+          </div>
         </div>
       </div>
-    </div>
+      {isDeleteModalOpen && <DeletePostModal toggleModal={toggleModal}/>}
+    </>
   );
 };
 
