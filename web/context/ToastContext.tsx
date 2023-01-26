cc2c97125624b33fd78dbe6cc4f52fc6328b0644
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-26 08:52:41 -0500

    Creating 'Chat' and 'Toast' contexts

---
+++ web/context/ToastContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, FC, useCallback, useContext, useState } from "react";
+
+interface Props {
+  children: JSX.Element[] | JSX.Element;
+}
+
+// Create an instance of React Context
+export const ToastContext = createContext({});
+
+// Provide ability to spawn Toast notification globally within the application
+export const ToastProvider: FC<Props> = ({ children }) => {
+  const [toastVisible, setToastVisible] = useState(false);
+  const [toastParams, setToastParams] = useState({});
+
+  // Set time that toast is visible
+  const durationMilliseconds = 3000;
+
+  // Call this function from any component when a toast message needs to be displayed.
+  const showToast = useCallback((type: string, message: string) => {
+    setToastVisible(true);
+
+    setToastParams({
+      type: type ? type : "error",
+      message: message ? message : "",
+    });
+
+    // Provide safe defaults if there is no message of type explicitly set
+    setTimeout(() => {
+      setToastVisible(false);
+    }, durationMilliseconds);
+  }, []);
+
+  return (
+    // showToast is the main part of the context value that needs to be accessible by all components. The other two values are used by the Toast component in _app.tsx only
+    <ToastContext.Provider
+      value={{ showToast, toastVisible, setToastVisible, toastParams }}
+    >
+      {children}
+    </ToastContext.Provider>
+  );
+};
+
+// Custom hook to allow components to access the context
+export const useToastContext = () => {
+  const context = useContext(ToastContext);
+
+  // Can optionally add conditional here to ensure toast context is used only by those components wrapped in a toast context provider
+
+  return context;
+};
