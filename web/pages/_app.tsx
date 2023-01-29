a1c8593884b0a95059f7323ccc86a9ddd40188ee
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-28 19:08:47 -0500

    Adding conditional rendering to _app.tsx

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -6,17 +6,29 @@ import { Toast } from "../components/ui";
 import { ToastContext, useToastContext } from "../context/ToastContext";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, ...appProps }: AppProps) {
   const { toastVisible, toastParams }: any = useToastContext();
+
+  let children = (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+
+  if (
+    ["/login"].includes(appProps.router.pathname) ||
+    ["/register"].includes(appProps.router.pathname)
+  ) {
+    // Excluding the 'Layout' component for the login and register pages to prevent the rendering of the 'Aside' component
+    children = <Component {...pageProps} />;
+  }
 
   return (
     <>
       <Head>
         <title>Capitlize | Talk with your friends!</title>
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      {children}
       <Toast visible={toastVisible} params={toastParams} />
     </>
   );
