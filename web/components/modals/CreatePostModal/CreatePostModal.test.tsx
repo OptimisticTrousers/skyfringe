371a4e56f76127b9a4f9402bdc5a774148c625d2
OptimisticTrousers <tony.isern1@gmail.com>
2023-02-03 09:18:38 -0500

    Writing test for 'CreatePostModal'

---
+++ web/components/modals/CreatePostModal/CreatePostModal.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import CreatePostModal from "./CreatePostModal";
+
+describe("CreatePostModal component", () => {
+  test("Disables post button by default", () => {
+    const toggleModal = jest.fn();
+
+    render(<CreatePostModal toggleModal={toggleModal} />);
+
+    const postButton = screen.getByRole("button", { name: /post/i });
+    expect(postButton).toHaveAttribute("disabled");
+  });
+  test("Enables post button once user enters post text", () => {
+    const toggleModal = jest.fn();
+    render(<CreatePostModal toggleModal={toggleModal} />);
+
+    const postButton = screen.getByRole("button", { name: /post/i });
+    const input = screen.getByRole("textbox");
+    userEvent.type(input, "test");
+
+    expect(postButton).not.toHaveAttribute("disabled");
+  });
+});
