7f78dec54f855e122736dfb54c53f4914d6d1dc7
OptimisticTrousers <tony.isern1@gmail.com>
2023-02-02 22:15:51 -0500

    Creating tests for CreatePost

---
+++ web/components/common/CreatePost/CreatePost.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  getByLabelText,
+  getByPlaceholderText,
+  render,
+  screen,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("CreatePost component", () => {
+  test("opening modal", () => {
+    const button
+  })
+})
