323845133d210a5c3fc5b79869203f3a2a5d5d14
OptimisticTrousers <tony.isern1@gmail.com>
2023-02-03 08:14:23 -0500

    Writing tests for CreatePost

--- web/components/common/CreatePost/CreatePost.test.tsx
+++ web/components/common/CreatePost/CreatePost.test.tsx
@@ -1,27 +1,23 @@
 import React from "react";
-import {
-  getByLabelText,
-  getByPlaceholderText,
-  render,
-  screen,
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CreatePost from "./CreatePost";
 
-describe("CreatePost component", async () => {
-  test("opening modal", () => {
+describe("CreatePost component", () => {
+  test("Hides modal by default", () => {
+    render(<CreatePost />);
 
-    // Arrange
-    const toggleModal = jest.fn();
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+  });
+  test("Shows modal when clicking create post button", async () => {
+    render(<CreatePost />);
 
-    // Act
-    const { getByText } = render(<CreatePost />);
+    // Click the create post button to reveal the add post form
+    const createPostButton = screen.getByRole("button");
 
-    // Assert
-    expect(getByText("Create Post")).toBeTruthy();
+    await userEvent.click(createPostButton);
 
-    // Act
-    const user = userEvent.setup()
-    await user.click(getByText())
+    const createPostModal = screen.getByRole("dialog");
+    expect(createPostModal).toBeInTheDocument();
   });
 });
