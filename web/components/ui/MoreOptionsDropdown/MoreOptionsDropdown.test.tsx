e9a7946cc0e5b00999521f1a44ead29ee69083bb
OptimisticTrousers <tony.isern1@gmail.com>
2023-02-06 16:02:58 -0500

    Writing 'useMenuCloseEvents' and other pages

--- web/components/ui/MoreOptionsDropdown/MoreOptionsDropdown.test.tsx
+++ web/components/ui/MoreOptionsDropdown/MoreOptionsDropdown.test.tsx
@@ -1,3 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import MoreOptionsDropdown from "./MoreOptionsDropdown";
+
+describe("MoreOptionsDropdown component", () => {
+  test("Calls deletePost function on when clicking the delete option");
+  const deletePostMock = jest.fn();
+  render(
+    <MoreOptionsDropdown
+      closeMenu={jest.fn}
+      handleDelete={deletePostMock}
+      handleEdit={jest.fn}
+    />
+  );
+
+  const deleteButton = screen.getByRole("menuitem", { name: /delete/i });
+  userEvent.click(deleteButton);
+
+  expect(deletePostMock).toHaveBeenCalledTimes(1);
+});
---
+++ web/hooks/useMenuCloseEvents.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+
+// Use this hook for any menu to apply common 'expected' UX features such as closing outside click or escape key press. Takes ID of the element to apply these events to, and the function that closes/dismissed the element
+const useMenuCloseEvents = (elementId: string, closeElement) => {
+  // Effect to apply event listeners once on initial component mount
+
+  useEffect(() => {
+    // Ensure the element closes when the user clicks any element outside the target element
+    const handleOutsideClick = (event) => {
+      if (event.target.id !== elementId) {
+        closeElement();
+      }
+    };
+
+    // Ensure the element closes when the user presses the Escape key
+    const handleEscPress = (event) => {
+      if (event.key === "Escape") {
+        closeElement();
+      }
+    };
+
+    window.addEventListener("click", handleOutsideClick);
+    window.addEventListener("keydown", handleEscPress);
+
+    // Clean up event listeners on component dismount
+    return () => {
+      window.removeEventListener("click", handleOutsideClick);
+      window.removeEventListener("keydown", handleEscPress);
+    };
+  }, [closeElement, elementId]);
+};
