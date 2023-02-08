0860e69fb2495a55dfba2a2490b6cda9734668bb
OptimisticTrousers <tony.isern1@gmail.com>
2023-02-08 09:25:03 -0500

    Adding types to the 'useMenuCloseEvents' hook

--- web/hooks/useMenuCloseEvents.tsx
+++ web/hooks/useMenuCloseEvents.tsx
@@ -1,19 +1,19 @@
 import { useEffect } from "react";
 
 // Use this hook for any menu to apply common 'expected' UX features such as closing outside click or escape key press. Takes ID of the element to apply these events to, and the function that closes/dismissed the element
-const useMenuCloseEvents = (elementId: string, closeElement) => {
+const useMenuCloseEvents = (elementId: string, closeElement: any) => {
   // Effect to apply event listeners once on initial component mount
 
   useEffect(() => {
     // Ensure the element closes when the user clicks any element outside the target element
-    const handleOutsideClick = (event) => {
+    const handleOutsideClick = (event: any) => {
       if (event.target.id !== elementId) {
         closeElement();
       }
     };
 
     // Ensure the element closes when the user presses the Escape key
-    const handleEscPress = (event) => {
+    const handleEscPress = (event: any) => {
       if (event.key === "Escape") {
         closeElement();
       }
@@ -29,3 +29,5 @@ const useMenuCloseEvents = (elementId: string, closeElement) => {
     };
   }, [closeElement, elementId]);
 };
+
+export default useMenuCloseEvents;
