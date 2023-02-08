8421c4776392264be415f0aeb410ad00ebc189e7
OptimisticTrousers <tony.isern1@gmail.com>
2023-02-08 09:45:46 -0500

    Writing the 'ThemeContext'

---
+++ web/context/ThemeContext.tsx
@@ -0,0 +1,34 @@
+import { FC, useState, createContext } from "react";
+
+interface Props {
+  children: JSX.Element[] | JSX.Element;
+}
+
+interface ThemeContext {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+// Create an instance of React Context
+export const ThemeContext = createContext<ThemeContext>({} as ThemeContext);
+
+export const ThemeProvider: FC<Props> = ({ children }) => {
+  const [theme, setTheme] = useState("");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => {
+      if (prevTheme === "light") {
+        return "dark";
+      } else if (prevTheme === "dark") {
+        return "light";
+      }
+      return "dark";
+    });
+  };
+
+  return (
+    <ThemeContext.Provider value={{ toggleTheme, theme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
