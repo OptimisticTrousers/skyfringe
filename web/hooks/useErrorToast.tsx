e19eaa416016078bb45b54719d9c02ed55d2cf82
OptimisticTrousers <tony.isern1@gmail.com>
2023-02-16 11:39:37 -0500

    Fixing the 'useErrorToast' hook

--- web/hooks/useErrorToast.tsx
+++ web/hooks/useErrorToast.tsx
@@ -1,18 +1,16 @@
-import { useEffect } from "react"
-import { useToastContext } from "../context/ToastContext"
+import { useEffect } from "react";
+import { useToastContext } from "../context/ToastContext";
 
 // Show an error toast with a fixed, non-specific message whenever a designated error property is set to true (e.g. on data fetch or post)
 
-interface Error {
-  errorMsg:
-}
-
-const useErrorToast = (error, errorMsg: string) => {
-  const {showToast} = useToastContext();
+const useErrorToast = (error: any, errorMsg: string) => {
+  const { showToast }: any = useToastContext();
 
   useEffect(() => {
-    if(error) {
-      showToast("error", errorMsg)
+    if (error) {
+      showToast("error", errorMsg);
     }
-  }, [error, errorMsg, showToast])
-}
+  }, [error, errorMsg, showToast]);
+};
+
+export default useErrorToast;
