013cb5e0559713184661ac2763c44472fed247df
OptimisticTrousers <tony.isern1@gmail.com>
2023-02-18 15:48:53 -0500

    Changing 'User' model and creating a 'seed' file to generate fake data

---
+++ server/seeds.ts
@@ -0,0 +1,36 @@
+import { faker } from "@faker-js/faker";
+import mongoConfig from "./src/config/db";
+import Post from "./src/models/post";
+import User from "./src/models/user";
+import Comment from "./src/models/comment";
+
+const createRandomUser = () => {
+  return new User({
+    fullName: faker.name.fullName(),
+    userName: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  });
+};
+
+const seed = async () => {
+  try {
+    mongoConfig();
+    console.log("Resetting database...");
+    await Promise.all([
+      User.deleteMany({}),
+      Post.deleteMany({}),
+      Comment.deleteMany({}),
+    ]);
+    const user1 = createRandomUser();
+    const user2 = createRandomUser();
+
+    await Promise.all([user1.save(), user2.save()]);
+    console.log("Done!");
+    process.exit(0);
+  } catch (error) {
+    console.error(`An error occured while seeding the database: ${error}`);
+  }
+};
+
+seed();
--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -7,15 +7,21 @@ const UserSchema = new Schema(
     fullName: { type: String, required: true },
     userName: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, minLength: 8, required: true },
-    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    photo: {
-      photoId: String,
-      imageUrl: String,
-      altText: String,
-    },
-    outgoingFriendRequests: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    incomingFriendRequests: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    password: { type: String, minLength: 8 },
+    // friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    // photo: {
+    //   photoId: String,
+    //   imageUrl: String,
+    //   altText: String,
+    // },
+    // cover: {
+    //   photoId: String,
+    //   imageUrl: String,
+    //   altText: String,
+
+    // },
+    // outgoingFriendRequests: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    // incomingFriendRequests: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
