d9e0c2239502ded79af649c03a178f67d3cac1c9
OptimisticTrousers <tony.isern1@gmail.com>
2023-02-20 18:02:14 -0500

    Changing npm script for server and doing 'npm install'

--- server/src/__tests__/registerUser.test.ts
+++ server/src/__tests__/registerUser.test.ts
@@ -4,34 +4,76 @@ import app from "../app";
 import initializeMongoServer from "../config/mongoConfigTesting";
 
 // Import db setup and teardown
-import "../config/mongoSetup";
+/*
+
+      "fullName": "Bob Jones",
+      "userName": "bobjones",
+      "email": "bobjones@gmail.com",
+      "password": "bobjones",
+
+*/
 
 describe("POST /register", () => {
+  beforeAll(() => {
+    initializeMongoServer()
+  })
   it("returns user object after successful user sign up/register (with password hidden)", async () => {
-    const res = await request(app).post("/api/auth/register").send({
+    const response = await request(app).post("/api/auth/register").send({
       fullName: "Bob Jones",
       userName: "bobjones",
       email: "bobjones@gmail.com",
       password: "bobjones",
     });
-    expect(res.headers["content-type"]).toMatch(/json/);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body.user.fullName).toBe("Bob Jones");
-  });
-  test("logs the")
-  test("user makes an account with an email that already exists", (done) => {
-    const user = {
-      firstName: "Bob",
-      lastName: "Jones",
+    expect(response.headers["Content-Type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+    expect(response.body.user).toEqual({
+      fullName: "Bob Jones",
+      userName: "bobjones",
       email: "bobjones@gmail.com",
       password: "bobjones",
-    };
-    request(app)
-      .post("/api/auth/register")
-      .send({ user })
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(400, done);
+    });
   });
-  test("user creates an account");
+  it("returns JWT token as json", async () => {
+    const response = await request(app).post("/api/auth/register").send({
+      fullName: "Bob Jones",
+      userName: "bobjones",
+      email: "bobjones@gmail.com",
+      password: "bobjones",
+    });
+    expect(response.headers["Content-Type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+    // Using jwt.io to find the result of the JWT token
+    expect(response.body.token).toBe(
+      "https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmdWxsTmFtZSI6IkJvYiBKb25lcyIsInVzZXJOYW1lIjoiYm9iam9uZXMiLCJlbWFpbCI6ImJvYmpvbmVzQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiYm9iam9uZXMifQ.a-baM2I_--oYBV4cZee8lxAMc9Ac0Wa-CUrbmqqlcv4"
+    );
+  });
+  it("it attaches the JWT token in the 'Set-Cookie' header", async () => {
+    const response = await request(app).post("/api/auth/register").send({
+      fullName: "Bob Jones",
+      userName: "bobjones",
+      email: "bobjones@gmail.com",
+      password: "bobjones",
+    })
+    expect(response.headers["Content-Type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+    expect(response.headers["Set-Cookie"]).toBe(null)
+  })
+  // test("the user is authenticated once they register", () => {
+
+  // });
+  // test("user makes an account with an email that already exists", (done) => {
+  //   const user = {
+  //     firstName: "Bob",
+  //     lastName: "Jones",
+  //     email: "bobjones@gmail.com",
+  //     password: "bobjones",
+  //   };
+  //   request(app)
+  //     .post("/api/auth/register")
+  //     .send({ user })
+  //     .set("Accept", "application/json")
+  //     .expect("Content-Type", /json/)
+  //     .expect(400, done);
+  // });
+  // test("user creates an account");
 });
