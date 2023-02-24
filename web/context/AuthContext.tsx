1b4e04a2cc3401abe6778c3dc28a0ce71b903b47
OptimisticTrousers <tony.isern1@gmail.com>
2023-02-23 19:49:22 -0500

    Checking if the user is authenticated

--- web/context/AuthContext.tsx
+++ web/context/AuthContext.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import { createContext, FC, useReducer } from "react";
 import useCurrentUser from "../hooks/useCurrentUser";
 import { User } from "../types";
@@ -32,6 +33,30 @@ const authReducer = (state: AuthState, action: Action) => {
       return state;
   }
 };
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const token = context.req.cookies["jwt"];
+
+  const response = await fetch(process.env.SERVER_HOST + "/api/profile", {
+    headers: {
+      cookie: context.req.rookies,
+    },
+  });
+
+  const data = await response.json();
+
+  if (!data.hasOwnProperty("user")) {
+    return {
+      redirect: {
+        destination: "/login",
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export const AuthContext = createContext({} as AuthContext);
 
