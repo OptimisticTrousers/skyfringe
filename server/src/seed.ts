9b75fce9418d5d6dd47b189af5a65f3db1066abf
OptimisticTrousers <tony.isern1@gmail.com>
2023-03-01 21:05:36 -0500

    Changing seed file

--- server/src/seed.ts
+++ server/src/seed.ts
@@ -3,14 +3,15 @@ import User from './models/user';
 import Post from './models/post';
 import Comment from './models/comment';
 import mongoConfig from './config/db';
+import mongoose from 'mongoose';
 
-async function seed() {
+async function seed(config: any) {
   if (process.env.NODE_ENV !== 'test') {
     throw new Error('NODE_ENV must equal "test"!');
   }
 
   try {
-    await mongoConfig();
+    config();
 
     console.log('Resetting database...');
     await Promise.all([
@@ -19,27 +20,36 @@ async function seed() {
       Comment.deleteMany({}),
     ]);
 
+    const luffyId = new mongoose.Types.ObjectId("63ffe2394ba6931001673a16");
+    const zoroId = new mongoose.Types.ObjectId("63ffe28a03861eacdf3b618d")
+    const namiId = new mongoose.Types.ObjectId("63ffe29611ff85f225e99d8d")
+    const usoppId = new mongoose.Types.ObjectId("63ffe2ad135076c12765b2c1")
+    const sanjiId = new mongoose.Types.ObjectId("63ffe2bf437a932c8f0a29c2")
+    const chopperId = new mongoose.Types.ObjectId("63ffe2cf76e04ac74aeb2ad9")
+    const robinId = new mongoose.Types.ObjectId("63ffe2dd7f606102f2089563")
+    const frankyId = new mongoose.Types.ObjectId("63ffe3abe057bf774d4f3343")
+    const brookId = new mongoose.Types.ObjectId("63ffe3b6ce9d3bc87e11beab")
+    const jinbeiId = new mongoose.Types.ObjectId("63ffe3c419931753b2effed0")
+
     console.log('Seeding users...');
 
     const passwordHash = await bcrypt.hash('password', 11);
+    // Set up array of user/post/comment docs to be later saved to db
+    const users = [
+      {
+        fullName: "Monkey D. Luffy",
+        userName: "luffy",
+        password: "",
+        email: "luffy@onepiece.com",
+        friends: [
+          {
 
-    const exampleUser = new User({
-      name: 'Example Test',
-      email: 'example@example.com',
-      username: 'example',
-      passwordHash,
-      profilePicture: 'tckqw4nglwleb7ngz8qz',
-      isEmailVerified: true,
-    });
+          }
+        ]
+      }
+    ]
 
-    const seedUser = new User({
-      name: 'Jonathan Seed',
-      email: 'seed@example.com',
-      username: 'jonathanseed',
-      passwordHash,
-      profilePicture: 'bhfuf041l0daetumeaxj',
-      isEmailVerified: true,
-    });
+    users.forEach
 
     // Save seeded users
     await Promise.all([exampleUser.save(), seedUser.save()]);
@@ -51,4 +61,4 @@ async function seed() {
   }
 }
 
-seed();
+seed(mongoConfig);
