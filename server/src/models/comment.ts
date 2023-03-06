4b2c5a919df311a5233ab53fcab851bc132283e3
OptimisticTrousers <tony.isern1@gmail.com>
2023-03-05 19:52:12 -0500

    Fixing Post model from the backend

--- server/src/models/comment.ts
+++ server/src/models/comment.ts
@@ -6,7 +6,6 @@ const CommentSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     content: { type: String, required: true },
-    replies: { type: Schema.Types.ObjectId, ref: "Comment" },
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
