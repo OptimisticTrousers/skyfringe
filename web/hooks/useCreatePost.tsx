211712930ad696bc8c583091f4f568ba57032cbf
OptimisticTrousers <tony.isern1@gmail.com>
2023-03-17 21:34:56 -0400

    Creating a hook called 'useCreatePost' that handles creating posts

---
+++ web/hooks/useCreatePost.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import useHttp from "./useHttp";
+
+const useCreatePost = () => {
+  const [response, setResponse] = useState(null);
+
+  const { post, loading, error } = useHttp();
+
+  // Accepts FormData object that may contain both text and images
+  const createPost = async (formData: FormData) => {
+    setResponse(null);
+
+    const data = await post(`${process.env.NEXT_PUBLIC_API_DOMAIN}`, formData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      credentials: "include",
+    });
+    setResponse(data);
+  };
+
+  return { createPost, response, loading, error };
+};
+
+export default useCreatePost;
