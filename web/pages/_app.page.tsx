c61635d8930b87c55120d2aa39d45af5d04d8dc0
OptimisticTrousers <tony.isern1@gmail.com>
2023-03-17 21:01:43 -0400

    Removing routes for authentication

--- web/pages/_app.page.tsx
+++ web/pages/_app.page.tsx
@@ -41,7 +41,7 @@ export default function App({ Component, pageProps, ...appProps }: AppProps) {
       <AuthProvider>
         <ToastProvider>
           <ThemeProvider>
-            <AuthRoutes>{children}</AuthRoutes>
+            {children}
           </ThemeProvider>
         </ToastProvider>
       </AuthProvider>
