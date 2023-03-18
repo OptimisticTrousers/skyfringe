f6e0056c46579a34c6a85f7e158d59eb52118563
OptimisticTrousers <tony.isern1@gmail.com>
2023-03-17 21:24:23 -0400

    Fetching data

---
+++ web/pages/index.page.tsx
@@ -0,0 +1,68 @@
+import CSSModules from "react-css-modules";
+import { SkeletonPost } from "../components/skeletons";
+import {
+  TopMenu,
+  CreatePost,
+  Post,
+  SideFooter,
+  Suggestions,
+} from "../components/common";
+import styles from "../styles/Home.module.css";
+import { GetServerSidePropsContext } from "next";
+import { Post as PostInterface } from "../types";
+import { FC } from "react";
+import useCurrentUser from "../hooks/useCurrentUser";
+import { HomeFriends } from "../components/friends";
+
+interface Props {
+  posts: PostInterface[];
+}
+
+const Home: FC<Props> = ({ posts }) => {
+
+  const renderedPosts = posts.map((post) => {
+    return <Post key={post._id} post={post} />;
+  });
+
+  return (
+    <>
+      <div styleName="home">
+        <div styleName="home__center">
+          <div styleName="home__menu">
+            <TopMenu />
+          </div>
+          <HomeFriends />
+          <CreatePost />
+          {/* <SkeletonPost /> */}
+          {renderedPosts}
+        </div>
+        <aside styleName="home__right">
+          <Suggestions />
+          <SideFooter />
+        </aside>
+      </div>
+    </>
+  );
+};
+
+export async function getServerSideProps() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/posts`);
+  const { posts } = await response.json();
+
+  if (!posts) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default CSSModules(Home, styles, {
+  allowMultiple: true,
+  handleNotFoundStyleName: "ignore",
+});
