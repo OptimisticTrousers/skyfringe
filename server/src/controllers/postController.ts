aec17cde201dbd8fdbd10f794ef6a5bc3477a5fd
OptimisticTrousers <tony.isern1@gmail.com>
2023-03-18 10:56:15 -0400

    Writing handler for POST request for creating a post

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import Post from "../models/post";
+import { body, validationResult } from "express-validator";
 
 export const post_list = (req: Request, res: Response, next: NextFunction) => {
   Post.find({})
@@ -14,9 +15,54 @@ export const post_list = (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
-export const post_create = []
+// @desc    Add new post
+// @route   POST /api/posts
+// @access  Private
+export const post_create = [
+  // Check for either post text or image upload to allow a user to post image only or text only, but not a post with neither
+  body("content").custom((value, { req }) => {
+    if ((!value || value.trim().length === 0) && !req.file) {
+      // neither text nor image has been provided
+      throw new Error("Post text or image is required");
+    }
+    // User has included one of either text or image. Continue with request handling
+    return true;
+  }),
+  // Process request after validation and sanitization
+  (req: any, res: Response, next: NextFunction) => {
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+    // Create new post
+    const post = new Post({
+      user: "6405360a990e06c35a62b94a", // req.user is created by the auth middle when accessing any protected route
+      content: req.body.content,
+      likes: [],
+      comments: [],
+      image: req.file && {
+        imageId: req.file.filename,
+        imageUrl: req.file.path,
+        altText: "bob",
+      },
+    });
 
-export const post_detail = (req: Request, res: Response, next: NextFunction) => {
+    if (!errors.isEmpty()) {
+      // There are errors.
+      return res.status(400).json({ errors: errors.array() });
+    }
+    post.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).json({ post });
+    });
+  },
+];
+
+export const post_detail = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   Post.findById(req.params.postId)
     .populate("author")
     .exec()
@@ -28,8 +74,10 @@ export const post_detail = (req: Request, res: Response, next: NextFunction) => 
     });
 };
 
-export const post_update = []
+export const post_update = [];
 
-export const post_delete = (req: Request, res: Response, next: NextFunction) => {
-
-}
+export const post_delete = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
