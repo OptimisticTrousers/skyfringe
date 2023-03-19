f02a2898b7e54586b9aa2df72b186e320e4ec1a1
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2023-03-19 03:03:59 -0400

    Writing tests for logging in the user

--- server/src/__tests__/loginUser.test.ts
+++ server/src/__tests__/loginUser.test.ts
@@ -1,13 +1,22 @@
 import request from "supertest";
-import assert from "assert";
 import app from "../app";
 
-describe("POST /login", () => {
-  test("user ", (done) => {
-    request(app)
-      .get("/user")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200, done);
+// Import db setup and teardown functionality
+import "../config/mongoSetupTesting";
+
+describe("login_user controller", () => {
+  it("should return a 403 status code when no user is found", async () => {
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send({ email: "bobjones@onepiece.com", password: "bobjones" });
+    expect(response.statusCode).toEqual(403);
+  });
+  it("returns user object after successful login", async () => {
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send({ email: "luffy@onepiece.com", password: "password" });
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.user.fullName).toEqual("Monkey D. Luffy");
   });
 });
