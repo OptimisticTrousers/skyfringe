7514ce6e3c973b0606f16e7d4e71e7b4e617a3d4
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2023-03-19 18:25:58 -0800

    Passing all tests for the 'getPosts' file

--- server/src/__tests__/getPost.test.ts
+++ server/src/__tests__/getPost.test.ts
@@ -1,6 +1,8 @@
 import request from "supertest";
 import express from "express";
+import mongoose from "mongoose";
 import { post_detail } from "../controllers/postController";
+import Post from "../models/post";
 
 // Setup new app instance
 const app = express();
@@ -11,13 +13,35 @@ app.get("/posts/:postId", post_detail);
 // Import db setup and teardown functionality
 import "../config/mongoSetupTesting";
 
-// Post ID for post by Luffy
-const postId = "4c8a331bda76c559ef000009";
-
 describe("post_detail controller", () => {
   it("should return a 404 if the post is not found", async () => {
-    const response = await request(app).get(`/posts/${postId}`).expect(404);
-
-    expect(response.body.message).toBe("Post not found");
+    // Post ID that is not in the database
+    const id = "4c8a331bda76c559ef000088";
+    const response = await request(app).get(`/posts/${id}`);
+    expect(response.status).toEqual(404);
+  });
+  it("should return a post if it exists", async () => {
+    // Luffy's Post ID
+    const id = "4c8a331bda76c559ef000009";
+    const response = await request(app).get(`/posts/${id}`);
+    expect(response.status).toEqual(200);
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.body.post.content).toBe(
+      "I'm going to be the King of the Pirates!"
+    );
+  });
+  it("returns the length of the amount of likes on the post", async () => {
+    // Luffy's Post ID
+    const id = "4c8a331bda76c559ef000009";
+    const response = await request(app).get(`/posts/${id}`);
+    expect(response.status).toEqual(200);
+    expect(response.body.post.likes.length).toEqual(3);
+  });
+  it("returns the length of the amount of comments on the post", async () => {
+    // Luffy's Post ID
+    const id = "4c8a331bda76c559ef000009";
+    const response = await request(app).get(`/posts/${id}`);
+    expect(response.status).toEqual(200);
+    expect(response.body.post.comments.length).toEqual(1);
   });
 });
