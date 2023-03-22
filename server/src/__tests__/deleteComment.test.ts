2023f9c168706b74ce2783ab706708a9f4fdf5a0
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2023-03-21 19:15:45 -1200

    Starting to write tests for deleting comments

---
+++ server/src/__tests__/deleteComment.test.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import request from "supertest"
+import { comment_delete } from "../controllers/commentController";
+
+// Setup new app instance
+const app = express();
+
+// Use the controller
+app.delete("/posts/:postId/comments/:commentId", comment_delete);
+
+// Import db setup and teardown functionality
+import "../config/mongoSetupTesting"
+
+describe("DELETE /posts/:postId/comments/:commentId", () => {
+  it("returns deleted comment ID ")
+})
