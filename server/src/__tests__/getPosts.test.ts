0f0bd71b42f7b5e546024039e67012e153d50cf0
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2023-03-25 20:39:14 -1200

    Writing an extra test for fetching posts

--- server/src/__tests__/getPosts.test.ts
+++ server/src/__tests__/getPosts.test.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import request from "supertest";
+import Post from "../models/post";
 import { post_list } from "../controllers/postController";
 
 // Setup new app instance
@@ -20,4 +21,44 @@ describe("GET /posts", () => {
     expect(Array.isArray(response.body.posts)).toEqual(true);
     expect(response.body.posts.length).toEqual(5);
   });
+  // it("should sort posts by createdAt in ascending order", async () => {
+  //   const post1 = new Post({
+  //     author: "4c8a331bda76c559ef000004",
+  //     content: "This is the first post",
+  //     likes: [],
+  //     photo: {
+  //       imageUrl: "https://example.com/image1.jpg",
+  //       altText: "A test image 1",
+  //     },
+  //     createdAt: new Date("2022-01-01T00:00:00.000Z"),
+  //   });
+  //   await post1.save();
+
+  //   const post2 = new Post({
+  //     author: "4c8a331bda76c559ef000004",
+  //     content: "This is the second post",
+  //     likes: [],
+  //     photo: {
+  //       imageUrl: "https://example.com/image2.jpg",
+  //       altText: "A test image 2",
+  //     },
+  //     createdAt: new Date("2022-01-02T00:00:00.000Z"),
+  //   });
+  //   await post2.save();
+
+  //   const response = await request(app).get("/posts");
+  //   expect(response.status).toBe(200);
+  //   expect(response.body.posts).toEqual([
+  //     expect.objectContaining({
+  //       _id: post1.id,
+  //       content: post1.content,
+  //       createdAt: post1.createdAt.toISOString(),
+  //     }),
+  //     expect.objectContaining({
+  //       _id: post2.id,
+  //       content: post2.content,
+  //       createdAt: post2.createdAt.toISOString(),
+  //     }),
+  //   ]);
+  // });
 });
