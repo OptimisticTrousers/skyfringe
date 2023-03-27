68c501f39e22c7ba20957b70d91be018181fb9ae
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2023-03-26 22:27:09 -1200

    Fixing syntax error

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/user";
 import Post from "../models/post";
-import Comment from "../models/comment";
 
 export const user_list = (
   req: Request,
@@ -28,33 +27,52 @@ export const user_feed = async (
   res: Response,
   next: NextFunction
 ) => {
-  const user = req.user._id as any;
-  const friends = req.user.friends as any;
+   const user = req.user as any;
 
-  const likedPosts = await Post.find({ likes: user })
-    .populate("author")
-    .populate("comments.author")
-    .sort("-createdAt");
-
-  const commentedPosts = await Comment.find({ author: user })
-    .populate("post")
+  // Find all posts from user's friends
+  const friendPosts = await Post.find({
+    author: { $in: user.friends },
+    is_reported: false,
+  })
+    .populate("author", "fullName userName photo")
     .populate({
-      path: "post",
-      populate: { path: "author", select: "fullName" },
-    })
-    .populate({
-      path: "post",
-      populate: { path: "comments", populate: { path: "author" } },
+      path: "comments",
+      populate: {
+        path: "author",
+        select: "fullName userName photo",
+      },
     })
     .sort("-createdAt");
 
-  const friendPosts = await Post.find({ author: { $in: friends } })
-    .populate("author")
-    .populate("comments.author")
+  // Find all posts where the user or their friends have commented
+  const commentedPosts = await Post.find({
+    $or: [
+      { author: user._id },
+      { author: { $in: user.friends } },
+      { "comments.author": { $in: user.friends } },
+      { "comments.author": user._id },
+    ],
+    is_reported: false,
+  })
+    .populate("author", "fullName userName photo")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "author",
+        select: "fullName userName photo",
+      },
+    })
     .sort("-createdAt");
 
-  const feed = likedPosts
-    .concat(commentedPosts.map((c: any) => c.post))
-    .concat(friendPosts);
-  res.json(feed);
+  // Merge the two arrays of posts and remove duplicates
+  const allPosts = [...friendPosts, ...commentedPosts];
+  const uniquePosts = allPosts.filter(
+    (post, index, self) =>
+      index ===
+      self.findIndex(
+        (p) => p._id.toString() === post._id.toString()
+      )
+  );
+ 
+  res.status(200).json({posts: uniquePosts})
 };
