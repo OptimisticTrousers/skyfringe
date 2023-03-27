ca7e57b07cc22875cdc01217eee2ad56bdd325e4
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2023-03-26 14:19:26 -1200

    Importing and using Toast component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,13 @@ import CSSModules from "react-css-modules";
 import { Outlet } from "react-router-dom";
 import { Aside } from "./components/common";
 import styles from "./assets/App.module.css";
+import { useContext } from "react";
+import { ToastContext } from "./context/ToastContext";
+import { Toast } from "./components/ui";
 
 const App = () => {
+  const { toastVisible, toastParams } = useContext(ToastContext);
+
   return (
     <>
       <div styleName="container">
@@ -13,11 +18,12 @@ const App = () => {
         </main>
       </div>
       <footer></footer>
+      <Toast visible={toastVisible} params={toastParams} />
     </>
   );
 };
 
 export default CSSModules(App, styles, {
   allowMultiple: true,
-  handleNotFoundStyleName: "ignore"
+  handleNotFoundStyleName: "ignore",
 });
