97820aabdb756be36ea9bc1f7ba8512cd62e37f3
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2023-03-26 22:01:16 -1200

    Writing a function to return the user's feed based on their friends posts, their own posts, posts that their friends have commented on, and posts that they have commented on

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,15 +1,75 @@
 import { Request, Response, NextFunction } from "express";
+import User from "../models/user";
+import Post from "../models/post";
 
-export const user_list = (req: Request, res: Response, next: NextFunction) => {
+export const user_list = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
 
-}
+export const user_detail = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
 
-export const user_detail = (req: Request, res: Response, next: NextFunction) => {
+export const user_update = [];
 
-}
+export const user_delete = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
 
-export const user_update = []
+export const user_feed = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = req.user as any;
 
-export const user_delete = (req: Request, res: Response, next: NextFunction) => {
-  
-}
+  // Find all posts from user's friends
+  const friendPosts = await Post.find({
+    author: { $in: user.friends },
+    is_reported: false,
+  })
+    .populate("author", "fullName userName photo")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "author",
+        select: "fullName userName photo",
+      },
+    })
+    .sort("-createdAt");
+
+  // Find all posts where the user or their friends have commented
+  const commentedPosts = await Post.find({
+    $or: [
+      { author: user._id },
+      { author: { $in: user.friends } },
+      { "comments.author": { $in: user.friends } },
+      { "comments.author": user._id },
+    ],
+    is_reported: false,
+  })
+    .populate("author", "fullName userName photo")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "author",
+        select: "fullName userName photo",
+      },
+    })
+    .sort("-createdAt");
+
+  // Merge the two arrays of posts and remove duplicates
+  const allPosts = [...friendPosts, ...commentedPosts];
+  const uniquePosts = allPosts.filter(
+    (post, index, self) =>
+      index === self.findIndex((p) => p._id.toString() === post._id.toString())
+  );
+
+  res.status(200).json({ posts: uniquePosts });
+};
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -4,6 +4,7 @@ import {
   user_detail,
   user_update,
   user_delete,
+  user_feed,
 } from "../controllers/userController";
 
 const router = Router();
@@ -11,5 +12,7 @@ const router = Router();
 router.route("/").get(user_list);
 
 router.route("/:userId").get(user_detail).put(user_update).delete(user_delete);
+
+router.route("/:userId/feed").get(user_feed)
 
 export default router;
