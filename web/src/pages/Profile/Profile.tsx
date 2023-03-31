0b9899aae89306177d5ccc9034da7562e2ef6b89
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2023-03-31 16:46:54 -0400

    Using data from the 'user' object in 'AuthContext'

--- web/src/pages/Profile/Profile.tsx
+++ web/src/pages/Profile/Profile.tsx
@@ -1,144 +1,151 @@
 import CSSModules from "react-css-modules";
+import { useContext } from "react";
 import { Post } from "../../components/common";
 import { Card } from "../../components/ui";
 import styles from "./Profile.module.css";
+import { AuthContext } from "../../context/AuthContext";
 
 const Profile = () => {
+  const { user } = useContext(AuthContext);
+  console.log(user);
   return (
-    <div styleName="profile">
-      <header styleName="profile__header">
-        <img src="/images/optimistictrousers.jpg" styleName="profile__image" />
-        <div styleName="profile__details">
+    user && (
+      <div styleName="profile">
+        <header styleName="profile__header">
           <img
             src="/images/optimistictrousers.jpg"
-            styleName="profile__avatar"
+            styleName="profile__image"
           />
-          <div styleName="profile__text">
-            <h2 styleName="profile__name">Samantha Nelson</h2>
-            <p>Product designer</p>
+          <div styleName="profile__details">
+            <img
+              src="/images/optimistictrousers.jpg"
+              styleName="profile__avatar"
+            />
+            <div styleName="profile__text">
+              <h2 styleName="profile__name">{user.fullName}</h2>
+              <h3 styleName="profile__username">@{user.userName}</h3>
+            </div>
           </div>
-        </div>
-      </header>
-      <div styleName="profile__content">
-        <aside styleName="profile__aside">
-          <Card>
-            <div styleName="profile__card">
-              <h3 styleName="profile__subtitle">About</h3>
-              <p styleName="profile__description">
-                Hi! My name is Samantha Nelson, I am a creative geek from
-                Prague. I enjoy creating eye canday solutions for web and mobile
-                apps
-              </p>
-            </div>
-          </Card>
-          <Card>
-            <div styleName="profile__card">
-              <h3 styleName="profile__subtitle">Media</h3>
-              <div styleName="profile__friends">
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
+        </header>
+        <div styleName="profile__content">
+          <aside styleName="profile__aside">
+            <Card>
+              <div styleName="profile__card">
+                <h3 styleName="profile__subtitle">About</h3>
+                <p styleName="profile__description">
+                  {user.bio ? user.bio : "Add a bio..."}
+                </p>
               </div>
-            </div>
-          </Card>
-          <Card>
-            <div styleName="profile__card">
-              <div styleName="profile__container">
-                <div styleName="profile__text">
-                  <h3 styleName="profile__subtitle">Friends</h3>
-                  <p styleName="profile__caption">10 friends</p>
+            </Card>
+            <Card>
+              <div styleName="profile__card">
+                <h3 styleName="profile__subtitle">Media</h3>
+                <div styleName="profile__friends">
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
                 </div>
-                <a styleName="profile__link">See all friends</a>
               </div>
-              <div styleName="profile__friends">
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
-                <img
-                  src="/images/optimistictrousers.jpg"
-                  styleName="profile__friend"
-                />
+            </Card>
+            <Card>
+              <div styleName="profile__card">
+                <div styleName="profile__container">
+                  <div styleName="profile__text">
+                    <h3 styleName="profile__subtitle">Friends</h3>
+                    <p styleName="profile__caption">{user.friendCount} friends</p>
+                  </div>
+                  <a styleName="profile__link">See all friends</a>
+                </div>
+                <div styleName="profile__friends">
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                  <img
+                    src="/images/optimistictrousers.jpg"
+                    styleName="profile__friend"
+                  />
+                </div>
               </div>
-            </div>
-          </Card>
-        </aside>
-        <div styleName="profile__main">
-          <ul styleName="profile__list">
-            <li styleName="profile__item">Your posts</li>
-            <li styleName="profile__item">Liked posts</li>
-            <li styleName="profile__item">Saved posts</li>
-          </ul>
-          <div styleName="profile__posts">
-            {/* <Post />
+            </Card>
+          </aside>
+          <div styleName="profile__main">
+            <ul styleName="profile__list">
+              <li styleName="profile__item">Your posts</li>
+              <li styleName="profile__item">Liked posts</li>
+              <li styleName="profile__item">Saved posts</li>
+            </ul>
+            <div styleName="profile__posts">
+              {/* <Post />
             <Post />
             <Post />
             <Post /> */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
