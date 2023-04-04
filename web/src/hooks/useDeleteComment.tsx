65129778a44920421863cd5784c3c87f6fcfd29d
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2023-04-04 17:44:46 -0400

    Writing a hook to delete comments

---
+++ web/src/hooks/useDeleteComment.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import useHttp from "./useHttp";
+
+const useDeleteComment = () => {
+  const { remove, data, loading, error } = useHttp();
+
+  // Requires both post and comment ID to ensure the API route is satisfied and that the comment is removed from the post's comments array.
+  const deleteComment = async (postId: string, commentId: string) => {
+    const response = await remove(
+      `${import.meta.env.VITE_API_DOMAIN}/posts/${postId}/comments/${commentId}`
+    );
+    return response;
+  };
+
+  return { deleteComment, data, loading, error };
+};
+
+export default useDeleteComment;
