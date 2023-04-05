fbc2a3dec2b0f9dabd5d299c428135ca7934bdeb
OptimisticTrousers <tony.isern@yahoo.com>
2023-04-05 15:38:21 -0400

    passing and running tests for deleting a comment

--- server/src/__tests__/deleteComment.test.ts
+++ server/src/__tests__/deleteComment.test.ts
@@ -1,16 +1,72 @@
 import express from "express";
 import request from "supertest";
+import errorHandler from "../middleware/errorHandler";
+import mockUser from "../middleware/mockUser";
 import { comment_delete } from "../controllers/commentController";
-
-// Setup new app instance
-const app = express();
-
-// Use the controller
-app.delete("/posts/:postId/comments/:commentId", comment_delete);
+import {
+  luffyCommentId,
+  luffyPostId,
+  zoroCommentId,
+  zoroPostId,
+} from "../config/populateDB";
 
 // Import db setup and teardown functionality
 import "../config/testSeeds";
 
+// Setup new app instance
+const app = express();
+// Use the controller
+app.delete("/posts/:postId/comments/:commentId", mockUser, comment_delete);
+// error handler
+app.use(errorHandler);
+
 describe("DELETE /posts/:postId/comments/:commentId", () => {
-  it("returns deleted comment ID successful delete operation");
+  it("returns deleted comment ID successful delete operation", async () => {
+    const response = await request(app).delete(
+      `/posts/${zoroPostId}/comments/${luffyCommentId}`
+    );
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+    expect(response.body._id).toBe(luffyCommentId.toString());
+  });
+  // it("throws an error if the post ID is not valid", async () => {
+  //   const response = await request(app).delete(
+  //     `/posts/${undefined}/comments/${luffyCommentId}`
+  //   );
+  //   expect(response.headers["content-type"]).toMatch(/json/);
+  //   expect(response.status).toEqual(400);
+  //   expect(response.body.message).toEqual("Invalid post ID");
+  // });
+  // it("throws an error if the post is not found", async () => {
+  //   const response = await request(app).delete(
+  //     `/posts/${randomId1}/comments/${luffyCommentId}`
+  //   );
+  //   expect(response.headers["content-type"]).toMatch(/json/);
+  //   expect(response.status).toEqual(400);
+  //   expect(response.body.message).toEqual("Post not found");
+  // });
+  // it("throws an error if the commentId is not valid", async () => {
+  //   const response = await request(app).delete(
+  //     `/posts/${luffyPostId}/comments/${undefined}`
+  //   );
+  //   expect(response.headers["content-type"]).toMatch(/json/);
+  //   expect(response.status).toEqual(400);
+  //   expect(response.body.message).toEqual("Invalid comment ID");
+  // });
+  // it("throws an error if the comment is not found", async () => {
+  //   const response = await request(app).delete(
+  //     `/posts/${luffyPostId}/comments/${randomId2}`
+  //   );
+  //   expect(response.headers["content-type"]).toMatch(/json/);
+  //   expect(response.status).toEqual(400);
+  //   expect(response.body.message).toEqual("Comment not found");
+  // });
+  it("returns a 403 error if the user is not the user who wrote the comment", async () => {
+    const response = await request(app).delete(
+      `/posts/${luffyPostId}/comments/${zoroCommentId}`
+    );
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(403);
+    expect(response.body.message).toEqual("Forbidden");
+  });
 });
--- server/src/controllers/commentController.ts
+++ server/src/controllers/commentController.ts
@@ -1,14 +1,29 @@
 import { Request, Response, NextFunction } from "express";
+import asyncHandler from "express-async-handler";
 import Comment from "../models/comment";
 
-export const comment_list = (req: Request, res: Response, next: NextFunction) => {
+export const comment_list = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
 
-}
+export const comment_create = [];
 
-export const comment_create = []
+export const comment_update = [];
 
-export const comment_update = []
+export const comment_delete = asyncHandler(async (req: any, res: Response) => {
+  const commentId = req.params.commentId;
 
-export const comment_delete = (req: Request, res: Response, next: NextFunction) => {
-  
-}
+  const comment = await Comment.findById(commentId).populate("author").exec();
+
+  if (comment && comment.author._id !== req.user._id) {
+    // it checks if the authenticated user ID matches the comment's author ID, and returns a 403 error if they don't match.
+    res.status(403).json({ message: "Forbidden" });
+    return;
+  }
+
+  const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+  res.status(200).json(deletedComment);
+});
