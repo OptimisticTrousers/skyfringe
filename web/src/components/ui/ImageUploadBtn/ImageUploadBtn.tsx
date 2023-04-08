3757235df7968aa933165743d8c89fdd87a88fbd
OptimisticTrousers <tony.isern1@gmail.com>
2023-04-08 18:42:34 -0400

    Creating a button component that handles the user adding an image file

---
+++ web/src/components/ui/ImageUploadBtn/ImageUploadBtn.tsx
@@ -0,0 +1,39 @@
+import CSSModules from "react-css-modules";
+import styles from "./ImageUploadBtn.module.css";
+import { FcGallery } from "react-icons/fc";
+
+// Carefully designed custom image file input. Due to being an input, no button role or features need to be added.
+const ImageUploadBtn = ({
+  handleChange,
+  imageValue,
+  setImageValue,
+  setImageFile,
+}: any) => {
+  return (
+    // Accepts only the formats specified
+    <>
+      <label htmlFor="image" styleName="file__label">
+        <input
+          type="file"
+          value={imageValue}
+          name="image"
+          id="image"
+          accept=".jpg, .jpeg, .png, .webp"
+          styleName="file__input"
+          onChange={(e: any) => {
+            setImageValue(e.target.value);
+            setImageFile(e.target.files[0]);
+            handleChange(e);
+          }}
+        />
+        <FcGallery styleName="file__icon" />
+        Add picture
+      </label>
+    </>
+  );
+};
+
+export default CSSModules(ImageUploadBtn, styles, {
+  allowMultiple: true,
+  handleNotFoundStyleName: "ignore"
+});
