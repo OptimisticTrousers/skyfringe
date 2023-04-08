7e4f7b0d498a69c3e0431d5bc851bcd2add79718
OptimisticTrousers <tony.isern1@gmail.com>
2023-04-08 18:41:54 -0400

    Creating a component to preview the image that the user has uploaded

---
+++ web/src/components/ui/ImagePreview/ImagePreview.tsx
@@ -0,0 +1,29 @@
+import CSSModules from "react-css-modules";
+import { SlClose } from "react-icons/sl";
+import styles from "./ImagePreview.module.css";
+
+const ImagePreview = ({
+  imageData,
+  setImageValue,
+  setImageFile,
+  removeThumbnail,
+}: any) => {
+  const handleClose = () => {
+    setImageValue("");
+    setImageFile(null);
+    removeThumbnail();
+  };
+  return (
+    <div styleName="preview">
+      <img styleName="preview__image" src={imageData} alt="" />
+      <button styleName="preview__button" type="button" onClick={handleClose}>
+        <SlClose styleName="preview__icon" />
+      </button>
+    </div>
+  );
+};
+
+export default CSSModules(ImagePreview, styles, {
+  allowMultiple: true,
+  handleNotFoundStyleName: "ignore",
+});
