10a7e6c2f9534bfa3d04e5b846cc135533c61d09
OptimisticTrousers <tony.isern1@gmail.com>
2023-04-15 15:05:14 -0400

    Getting rid of the useless 'Dropdown' component

--- web/src/components/ui/index.ts
+++ web/src/components/ui/index.ts
@@ -1,5 +1,4 @@
 export { default as Card } from "./Card";
-export { default as Dropdown } from "./Dropdown";
 export { default as MoreOptionsDropdown } from "./MoreOptionsDropdown";
 export { default as Notification } from "./Notification";
 export { default as Toast } from "./Toast";
