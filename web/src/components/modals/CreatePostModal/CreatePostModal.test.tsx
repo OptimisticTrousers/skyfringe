2f249167f31758b44c0d923d6412eb15c51edf19
OptimisticTrousers <tony.isern1@gmail.com>
2023-04-15 11:40:14 -0400

    Writing test for 'CreatePostModal'

--- web/src/components/modals/CreatePostModal/CreatePostModal.test.tsx
+++ web/src/components/modals/CreatePostModal/CreatePostModal.test.tsx
@@ -3,22 +3,52 @@ import userEvent from "@testing-library/user-event";
 import CreatePostModal from "./CreatePostModal";
 
 describe("CreatePostModal component", () => {
-  test("Disables post button by default", () => {
-    const toggleModal = jest.fn();
+  const setup = (jsx: JSX.Element) => {
+    return {
+      user: userEvent.setup(),
+      ...render(jsx),
+    };
+  };
 
-    render(<CreatePostModal toggleModal={toggleModal} />);
+  test("if modal title is 'Create Post'", () => {
+    const toggleModal = vi.fn();
+    setup(<CreatePostModal toggleModal={toggleModal} />);
+    expect(screen.getByText("Create Post")).toBeInTheDocument();
+  });
+
+  test("Disables post button by default", () => {
+    const toggleModal = vi.fn();
+    setup(<CreatePostModal toggleModal={toggleModal} />);
 
     const postButton = screen.getByRole("button", { name: /post/i });
     expect(postButton).toHaveAttribute("disabled");
   });
+
   test("Enables post button once user enters post text", () => {
-    const toggleModal = jest.fn();
-    render(<CreatePostModal toggleModal={toggleModal} />);
+    const toggleModal = vi.fn();
+    setup(<CreatePostModal toggleModal={toggleModal} />);
 
     const postButton = screen.getByRole("button", { name: /post/i });
     const input = screen.getByRole("textbox");
     userEvent.type(input, "test");
 
     expect(postButton).toHaveAttribute("disabled", "");
+  });
+  test("if modal is closed when 'Post' button is clicked", async () => {
+    const toggleModal = vi.fn();
+    const { user } = setup(<CreatePostModal toggleModal={toggleModal} />);
+    const postButton = screen.getByRole("button", { name: /post/i });
+    await user.click(postButton);
+    expect(toggleModal).toHaveBeenCalled();
+  });
+  test("if image upload button works correctly", async () => {
+    const toggleModal = vi.fn();
+    const {user} = setup(<CreatePostModal toggleModal={toggleModal} />);
+    const imageUploadButton = screen.getByLabelText("Add image");
+    const file = new File(["test image"], "images/optimistictrousers.png", {
+      type: "image/png",
+    });
+    user.upload(imageUploadButton, file)
+    expect(screen.getByAltText("Preview image")).toBeInTheDocument()
   });
 });
