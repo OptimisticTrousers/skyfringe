a9c39320ba3e0391f3876c4b232b8c35e7c31bf0
OptimisticTrousers <tony.isern1@gmail.com>
2023-04-15 11:24:01 -0400

    Changing implementation details for 'CreatePost'

--- web/src/components/common/CreatePost/CreatePost.test.tsx
+++ web/src/components/common/CreatePost/CreatePost.test.tsx
@@ -1,20 +1,27 @@
-import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
 import CreatePost from "./CreatePost";
 
 describe("CreatePost component", () => {
+  const setup = (jsx: JSX.Element) => {
+    return {
+      user: userEvent.setup(),
+      ...render(jsx),
+    };
+  };
+
   test("Hides modal by default", () => {
-    render(<CreatePost />);
+    setup(<CreatePost />);
 
     expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
   });
   test("Shows modal when clicking create post button", async () => {
-    render(<CreatePost />);
+    const { user } = setup(<CreatePost />);
 
     // Click the create post button to reveal the add post form
     const createPostButton = screen.getByRole("button");
 
-    await userEvent.click(createPostButton);
+    await user.click(createPostButton);
 
     const createPostModal = screen.getByRole("dialog");
     expect(createPostModal).toBeInTheDocument();
