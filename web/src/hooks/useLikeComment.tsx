ecae4ff8cfd3f35be416fedcd8289b490e89ac9b
OptimisticTrousers <tony.isern1@gmail.com>
2023-04-16 12:41:45 -0400

    Writing hook for liking a comment

---
+++ web/src/hooks/useLikeComment.tsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { ToastContext } from "../context/ToastContext";
+import useHttp from "./useHttp";
+
+const useLikeComment = () => {
+  const { put, data, loading, error } = useHttp();
+  const { showToast } = useContext(ToastContext);
+
+  const likeComment = async (commentId: string, postId: string) => {
+    const response = await put(
+      `${import.meta.env.VITE_API_DOMAIN}/posts/${postId}/comments/${commentId}`
+    );
+    if (response) {
+      showToast("success", "You have successfully updated a post!");
+    } else if (error) {
+      showToast("error", "An error occured while updating the post.");
+    }
+    return response;
+  };
+
+  return { likeComment, data, loading, error };
+};
+
+export default useLikeComment;
