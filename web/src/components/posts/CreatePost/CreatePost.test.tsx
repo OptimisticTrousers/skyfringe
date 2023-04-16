baad29be52f13a72e9611ba6628419ba6f9179da
OptimisticTrousers <tony.isern1@gmail.com>
2023-04-15 21:15:34 -0400

    Fixing tests for 'CreatePost'

--- web/src/components/posts/CreatePost/CreatePost.test.tsx
+++ web/src/components/posts/CreatePost/CreatePost.test.tsx
@@ -3,20 +3,14 @@ import { render, screen } from "@testing-library/react";
 import CreatePost from "./CreatePost";
 
 describe("CreatePost component", () => {
-  const setup = (jsx: JSX.Element) => {
-    return {
-      user: userEvent.setup(),
-      ...render(jsx),
-    };
-  };
-
   test("Hides modal by default", () => {
-    setup(<CreatePost />);
+    render(<CreatePost />);
 
     expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
   });
   test("Shows modal when clicking create post button", async () => {
-    const { user } = setup(<CreatePost />);
+    const user = userEvent.setup();
+    render(<CreatePost />);
 
     // Click the create post button to reveal the add post form
     const createPostButton = screen.getByRole("button");
