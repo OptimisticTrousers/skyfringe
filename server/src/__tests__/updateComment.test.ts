f938dfc5c0381223cd0c691860a6cb1357fbd94c
OptimisticTrousers <tony.isern@yahoo.com>
2023-04-17 19:20:39 -0400

    Writing and passing tests for updating a comment

--- server/src/__tests__/updateComment.test.ts
+++ server/src/__tests__/updateComment.test.ts
@@ -1,23 +1,59 @@
-import express from "express"
-import request from "supertest"
-import app from "../app";
+import express from "express";
+import request from "supertest";
+import mockUser from "../middleware/mockUser";
+import { comment_update } from "../controllers/commentController";
 
 // Import db setup and teardown functionality
 import "../config/testSeeds";
+import errorHandler from "../middleware/errorHandler";
+import { zoroCommentId } from "../config/populateDB";
 
-const luffyCommentId
+// Setup new app instance
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// Use the controller
+app.put("/comments/:commentId", mockUser, comment_update);
+// error handler
+app.use(errorHandler);
+
+const luffyCommentId = "4c8a331bda76c559ef000014";
 
 describe("PUT /api/comments/:commentId", () => {
-  it("should update a comment content'", async () => {
-    const newContent = 'Updated comment';
+  const newContent = "Updated comment";
+  it("should update a comment content", async () => {
     const response = await request(app)
-      .put(`/comments/${comment._id}`)
+      .put(`/comments/${luffyCommentId}`)
       .send({ content: newContent });
 
-    expect(response.statusCode).toBe(200);
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toBe(200);
     expect(response.body.content).toBe(newContent);
-  })
-  it("should add a like to a comment", async () => {
+  });
+  it("should return a 400 error if no object is sent", async () => {
+    const response = await request(app)
+      .put(`/comments/${luffyCommentId}`)
+      .send({});
 
-  })
-})
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toBe(400);
+    expect(response.body.errors[0].msg).toBe("Content is required");
+  });
+  it("should return a 400 error if content is empty", async () => {
+    const response = await request(app)
+      .put(`/comments/${luffyCommentId}`)
+      .send({ content: "" });
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toBe(400);
+    expect(response.body.errors[0].msg).toBe("Content is required");
+  });
+  it("returns a 403 error if the user is not the user who wrote the comment", async () => {
+    const response = await request(app)
+      .put(`/comments/${zoroCommentId}`)
+      .send({ content: newContent });
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(403);
+    expect(response.body.message).toEqual("Forbidden");
+  });
+});
--- server/src/controllers/commentController.ts
+++ server/src/controllers/commentController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import asyncHandler from "express-async-handler";
 import Comment from "../models/comment";
+import { body, validationResult } from "express-validator";
 
 export const comment_list = (
   req: Request,
@@ -10,7 +11,38 @@ export const comment_list = (
 
 export const comment_create = [];
 
-export const comment_update = [];
+// @desc    Update single post
+// @route   PUT /api/posts/:postId/comments/:commentId
+// @access  Private
+export const comment_update = [
+  body("content", "Content is required").trim().isLength({ min: 1 }).escape(),
+  asyncHandler(async (req: any, res: any, next: NextFunction) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+
+    const commentId = req.params.commentId;
+
+    const comment = await Comment.findById(commentId).exec();
+
+    if (comment && comment.author !== req.user._id) {
+      // it checks if the authenticated user ID matches the comment's author ID, and returns a 403 error if they don't match.
+      res.status(403).json({ message: "Forbidden" });
+      return;
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+      commentId,
+      { content: req.body.content },
+      { new: true }
+    );
+
+    res.status(200).json(updatedComment);
+  }),
+];
 
 // @desc    Delete single comment
 // @route   DELETE /api/posts/:postId/comments/:commentId
