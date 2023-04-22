63b65156509e527ca441e25ed7c403dcc18c83e7
OptimisticTrousers <tony.isern1@gmail.com>
2023-04-22 18:58:24 -0400

    Passing all tests for 'CreatePostModal'

--- web/src/components/modals/CreatePostModal/CreatePostModal.test.tsx
+++ web/src/components/modals/CreatePostModal/CreatePostModal.test.tsx
@@ -12,12 +12,6 @@ describe("CreatePostModal component", () => {
     };
   };
 
-  test("if modal title is 'Create Post'", () => {
-    const toggleModal = vi.fn();
-    setup(<CreatePostModal toggleModal={toggleModal} />);
-    expect(screen.getByText("Create Post")).toBeInTheDocument();
-  });
-
   test("Disables post button by default", () => {
     const toggleModal = vi.fn();
     setup(<CreatePostModal toggleModal={toggleModal} />);
@@ -57,51 +51,36 @@ describe("CreatePostModal component", () => {
     await user.upload(imageUploadButton, file);
     expect(screen.getByAltText("Preview image")).toBeInTheDocument();
   });
-  // test("if image is correctly overriden", async () => {
-  //   const toggleModal = vi.fn();
-  //   const { user } = setup(<CreatePostModal toggleModal={toggleModal} />);
+  test("if image is correctly overriden", async () => {
+    const toggleModal = vi.fn();
+    const user = userEvent.setup();
+    render(<CreatePostModal toggleModal={toggleModal} />);
 
-  //   const imageUploadButton = screen.getByLabelText(
-  //     "Add picture"
-  //   ) as HTMLInputElement;
-  //   const file1 = new File(["test image 1"], "optimistictrousers.jpg", {
-  //     type: "image/jpg",
-  //   });
-  //   expect(screen.queryByAltText("Preview image")).not.toBeInTheDocument();
-  //   // await user.upload(imageUploadButton, file1);
-  //   fireEvent.change(imageUploadButton, { target: { files: [file1] } });
-  //   expect(imageUploadButton.files![0]).toStrictEqual(file1);
-  //   // expect(imageUploadButton.files!.item(0)).toStrictEqual(file1);
-  //   expect(imageUploadButton.files).toHaveLength(1);
-  //   // expect(screen.getByAltText("Preview image")).toBeInTheDocument();
-  //   // expect(imageUploadButton.files!.item(0)).toStrictEqual(file1);
-  //   const file2 = new File(["test image 2"], "heart.png", {
-  //     type: "image/png",
-  //   });
-  //   // await user.upload(imageUploadButton, file2);
-  //   fireEvent.change(imageUploadButton, { target: { files: [file2] } });
+    const imageUploadButton = screen.getByLabelText(
+      "Add picture"
+    ) as HTMLInputElement;
+    const file1 = new File(["test image 1"], "optimistictrousers.jpg", {
+      type: "image/jpg",
+    });
 
-  //   expect(imageUploadButton.files![1]).toStrictEqual(file2);
-  //   // expect(imageUploadButton.files!.item(1)).toStrictEqual(file1);
-  //   expect(imageUploadButton.files).toHaveLength(1);
-  //   expect(screen.getByAltText("Preview image")).toBeInTheDocument();
-  // });
-  // test("that the image upload button displays an error message when the file is too big", async () => {
-  //   const toggleModal = vi.fn();
-  //   const { user } = setup(
-  //     <ToastProvider>
-  //       <CreatePostModal toggleModal={toggleModal} />
-  //     </ToastProvider>
-  //   );
-  //   const imageUploadButton = screen.getByLabelText("Add picture");
-  //   const file = new File(["test image"], "massive.jpg", {
-  //     type: "image/jpg",
-  //   });
-  //   await user.upload(imageUploadButton, file);
-  //   const toast = await screen.findByText("File is too big. Max size is 10 MB.");
-  //   expect(toast).toBeInTheDocument();
-  //   expect(screen.getByAltText("Preview image")).not.toBeInTheDocument();
-  // });
+    expect(screen.queryByAltText("Preview image")).not.toBeInTheDocument();
+    await user.upload(imageUploadButton, file1);
+    expect(imageUploadButton.files![0]).toStrictEqual(file1);
+    expect(imageUploadButton.files).toHaveLength(1);
+    expect(screen.getByAltText("Preview image")).toBeInTheDocument();
+
+    const file2 = new File(["test image 2"], "heart.png", {
+      type: "image/png",
+    });
+    const clearPhotoButton = screen.getByLabelText("Remove image");
+    await user.click(clearPhotoButton);
+    expect(screen.queryByAltText("Preview image")).not.toBeInTheDocument();
+
+    await user.upload(imageUploadButton, file2);
+    expect(imageUploadButton.files![0]).toStrictEqual(file1);
+    expect(imageUploadButton.files).toHaveLength(1);
+    expect(screen.getByAltText("Preview image")).toBeInTheDocument();
+  });
   test("that the image is removed once the user clicks the 'X' button", async () => {
     const toggleModal = vi.fn();
     const { user } = setup(<CreatePostModal toggleModal={toggleModal} />);
