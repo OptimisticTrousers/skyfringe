352ba9a97d4034b69c77abe448669a96cf15eccd
OptimisticTrousers <tony.isern1@gmail.com>
2023-04-22 22:46:11 -0400

    rendering user's name in the 'CreatePost' component

--- web/src/components/posts/CreatePost/CreatePost.test.tsx
+++ web/src/components/posts/CreatePost/CreatePost.test.tsx
@@ -1,16 +1,31 @@
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 import CreatePost from "./CreatePost";
+import { AuthContext } from "../../../context/AuthContext";
 
 describe("CreatePost component", () => {
+  const bobJones = {
+    _id: "4c8a331bda76c559ef000006",
+    fullName: "Bob Jones",
+    userName: "bobjones",
+    email: "bobjones@gmail.com",
+  };
   test("Hides modal by default", () => {
-    render(<CreatePost />);
+    render(
+      <AuthContext.Provider value={{ user: bobJones }}>
+        <CreatePost />
+      </AuthContext.Provider>
+    );
 
     expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
   });
   test("Shows modal when clicking create post button", async () => {
     const user = userEvent.setup();
-    render(<CreatePost />);
+    render(
+      <AuthContext.Provider value={{ user: bobJones }}>
+        <CreatePost />
+      </AuthContext.Provider>
+    );
 
     // Click the create post button to reveal the add post form
     const createPostButton = screen.getByRole("button");
@@ -19,5 +34,16 @@ describe("CreatePost component", () => {
 
     const createPostModal = screen.getByRole("dialog");
     expect(createPostModal).toBeInTheDocument();
+  });
+  test("renders correct text", () => {
+    render(
+      <AuthContext.Provider value={{ user: bobJones }}>
+        <CreatePost />
+      </AuthContext.Provider>
+    );
+
+    const message = screen.getByText("What's on your mind, bobjones? Write a post or upload a picture!");
+
+    expect(message).toBeInTheDocument();
   });
 });
--- web/src/components/posts/CreatePost/CreatePost.tsx
+++ web/src/components/posts/CreatePost/CreatePost.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import CSSModules from "react-css-modules";
 import { FcPlus } from "react-icons/fc";
+import { AuthContext } from "../../../context/AuthContext";
 import { CreatePostModal } from "../../modals";
 import { Card } from "../../ui";
 import styles from "./CreatePost.module.css";
 
 const CreatePost = () => {
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+
+  const { user } = useContext(AuthContext);
 
   const toggleModal = () => {
     setIsCreateModalOpen((prevValue) => !prevValue);
@@ -22,8 +25,8 @@ const CreatePost = () => {
             <div styleName="create__text">
               <h3 styleName="create__title">Create a Post</h3>
               <p styleName="create__description">
-                What&apos;s on your mind, Tony? Write a post or upload a
-                picture!
+                What&apos;s on your mind, {user.userName}? Write a post or
+                upload a picture!
               </p>
             </div>
           </div>
