f37fda4532f4d47eceb037eee391a41420e17e5d
OptimisticTrousers <tony.isern@yahoo.com>
2023-05-01 19:04:01 -0400

    Passing all tests for logging in a user

--- server/src/__tests__/loginUser.test.ts
+++ server/src/__tests__/loginUser.test.ts
@@ -1,6 +1,5 @@
 import request from "supertest";
 import app from "../app";
-
 // Import db setup and teardown functionality
 import "../config/testSeeds";
 
@@ -8,8 +7,29 @@ describe("login_user controller", () => {
   it("should return a 403 status code when no user is found", async () => {
     const response = await request(app)
       .post("/api/auth/login")
-      .send({ email: "bobjones@onepiece.com", password: "bobjones" });
-    expect(response.status).toEqual(403);
+      .send({ email: "bobjones@gmail.com", password: "bobjones" });
+    expect(response.status).toEqual(401);
+  });
+  it("should return 400 if validation fails", async () => {
+    const response = await request(app).post("/api/auth/register").send({
+      email: "invalid_email",
+      password: "",
+    });
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(400);
+    expect(response.body).toContainEqual({
+      msg: "Please enter a valid email address",
+      param: "email",
+      location: "body",
+      value: "invalid_email",
+    });
+    expect(response.body).toContainEqual({
+      msg: "Password must be at least 8 characters long",
+      param: "password",
+      location: "body",
+      value: "",
+    });
   });
   it("returns user object after successful login", async () => {
     const response = await request(app)
@@ -17,24 +37,9 @@ describe("login_user controller", () => {
       .send({ email: "zoro@onepiece.com", password: "password" });
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toEqual(200);
-    expect(response.body.user.fullName).toEqual("Roronoa Zoro");
-    expect(response.body.user.userName).toEqual("zoro");
-    expect(response.body.user.email).toEqual("zoro@onepiece.com");
-    expect(response.body.user.friends).toEqual([
-      "4c8a331bda76c559ef000004",
-      "4c8a331bda76c559ef000006",
-      "4c8a331bda76c559ef000007",
-    ]);
-  });
-  it("returns the Luffy guest user object if no 'email' or 'password' property is in the req.body", async () => {
-    const response = await request(app).post("/api/auth/login").send({});
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.status).toEqual(200);
-    expect(response.headers["set-cookie"]).toBeDefined();
-    expect(response.headers["set-cookie"][0]).toMatch(/jwt=/);
-    expect(response.body.user.fullName).toEqual("Monkey D. Luffy");
-    expect(response.body.user.userName).toEqual("luffy");
-    expect(response.body.user.email).toEqual("luffy@onepiece.com");
-    expect(response.body.user.friends).toEqual(["4c8a331bda76c559ef000005"]);
+    expect(response.body.fullName).toEqual("Roronoa Zoro");
+    expect(response.body.userName).toEqual("zoro");
+    expect(response.body.email).toEqual("zoro@onepiece.com");
+    expect(response.headers["set-cookie"]).toBeTruthy();
   });
 });
