1ffcda3b91cc7b07897f5c7cb588f4fa4b17e8ca
OptimisticTrousers <tony.isern@yahoo.com>
2023-05-02 01:31:40 -0400

    Fixing logout user

--- server/src/__tests__/logoutUser.test.ts
+++ server/src/__tests__/logoutUser.test.ts
@@ -1,29 +1,21 @@
 import request from "supertest";
-import app from "../app";
+import express from "express";
+import { logout_user } from "../controllers/authController";
 // Import db setup and teardown functionality
 import "../config/testSeeds";
 
-describe("GET /api/auth/logout", () => {
-  it("should return 401 if user is not logged in", async () => {
-    const response = await request(app).get("/api/auth/logout");
-    expect(response.status).toBe(401);
-    expect(response.body.message).toBe("User not logged in");
-  });
-  it("should logout the user", async () => {
-    const agent = request.agent(app);
-    const response = await agent
-      .post("/api/auth/login")
-      .send({ email: "luffy@onepiece.com", password: "password" });
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/logout", logout_user);
 
-    expect(response.headers["content-type"]).toMatch(/json/);
+describe("GET /api/auth/logout", () => {
+  it("should return 200 and clear jwt cookie if user is logged in", async () => {
+    const response = await request(app).get("/logout").set("Cookie", `jwt=jwt`);
     expect(response.status).toBe(200);
-    expect(response.headers["set-cookie"]).toBeTruthy();
-    const logoutResponse = await agent.get("/api/auth/logout");
-    expect(logoutResponse.headers["content-type"]).toMatch(/json/);
-    expect(logoutResponse.status).toBe(200);
-    expect(logoutResponse.body.message).toBe("User successfully logged out");
-    expect(logoutResponse.header["set-cookie"]).toEqual([
-      "jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly",
+    expect(response.body.message).toBe("User successfully logged out");
+    expect(response.header["set-cookie"]).toEqual([
+      "jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT",
     ]);
   });
 });
