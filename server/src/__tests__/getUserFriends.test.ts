d2dd72c232ee4a467338fa9a41563f66bc60fac9
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2023-05-03 01:06:02 -0400

    Fixing tests for fetching a user's friends

--- server/src/__tests__/getUserFriends.test.ts
+++ server/src/__tests__/getUserFriends.test.ts
@@ -1,12 +1,19 @@
+import express from "express";
 import request from "supertest";
-import app from "../app";
+import { post_list } from "../controllers/postController";
 import { luffyId } from "../config/populateDB";
+import { user_friends } from "../controllers/userController";
 // Import db setup and teardown functionality
 import "../config/testSeeds";
 
+// Setup new app instance
+const app = express();
+// Use the controller
+app.get("users/:userId/friends", user_friends);
+
 describe("GET /api/users/:userId/friends", () => {
   it("should return a list of user friends/friend requests", async () => {
-    const response = await request(app).get(`/api/users/${luffyId}/friends`);
+    const response = await request(app).get(`/users/${luffyId}/friends`);
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toBe(200);
     expect(Array.isArray(response.body)).toEqual(true);
