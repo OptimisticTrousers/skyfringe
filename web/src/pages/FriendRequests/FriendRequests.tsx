267493509a03511861d7092c99d25cee528e14ab
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2023-05-03 01:04:59 -0400

    Implementing all three 'friends' pages

--- web/src/pages/FriendRequests/FriendRequests.tsx
+++ web/src/pages/FriendRequests/FriendRequests.tsx
@@ -1,37 +1,76 @@
 import CSSModules from "react-css-modules";
 import styles from "../../assets/Friends.module.css";
 import { FriendsLayout, FriendsCard } from "../../components/friends";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import useFetch from "../../hooks/useFetch";
+import { FriendsErrorLoading } from "../../components/skeletons";
 
 const FriendRequests = () => {
+  const { user } = useContext(AuthContext);
+  const { data, loading, error }: any = useFetch(
+    `${import.meta.env.VITE_API_DOMAIN}/users/${user._id}/friends`
+  );
+
+  const incomingRequests = data?.friendRequests.filter(
+    (friendRequest: any) => friendRequest.status === "incoming"
+  );
+  const sentRequests = data?.friendRequests.filter(
+    (friendRequest: any) => friendRequest.status === "outgoing"
+  );
+
   return (
     <FriendsLayout>
       <section styleName="friends">
         <h2 styleName="friends__title">Incoming Requests</h2>
-        <ul styleName="friends__cards">
-          <li styleName="friends__card">
-            {/* <FriendsCard /> */}
-          </li>
-          <li styleName="friends__card">
-            {/* <FriendsCard /> */}
-          </li>
-          <li styleName="friends__card">
-            {/* <FriendsCard /> */}
-          </li>
-        </ul>
+        {incomingRequests ? (
+          <ul styleName="friends__cards">
+            {incomingRequests.length > 0 ? (
+              incomingRequests?.map((incomingRequest: any) => {
+                return (
+                  <li styleName="friends__card">
+                    <FriendsCard
+                      friend={incomingRequest.user}
+                      type="incoming"
+                    />
+                  </li>
+                );
+              })
+            ) : (
+              <li styleName="friends__message">No requests yet...</li>
+            )}
+          </ul>
+        ) : (
+          <FriendsErrorLoading
+            message="Unable to load requests"
+            loading={loading}
+            error={error}
+          />
+        )}
       </section>
       <section styleName="friends">
         <h2 styleName="friends__title">Sent requests</h2>
-        <ul styleName="friends__cards">
-          <li styleName="friends__card">
-            {/* <FriendsCard /> */}
-          </li>
-          <li styleName="friends__card">
-            {/* <FriendsCard /> */}
-          </li>
-          <li styleName="friends__card">
-            {/* <FriendsCard /> */}
-          </li>
-        </ul>
+        {incomingRequests ? (
+          <ul styleName="friends__cards">
+            {sentRequests.length > 0 ? (
+              sentRequests?.map((sentRequest: any) => {
+                return (
+                  <li styleName="friends__card">
+                    <FriendsCard friend={sentRequest.user} type="outgoing" />
+                  </li>
+                );
+              })
+            ) : (
+              <li styleName="friends__message">No requests sent...</li>
+            )}
+          </ul>
+        ) : (
+          <FriendsErrorLoading
+            message="Unable to load requests"
+            loading={loading}
+            error={error}
+          />
+        )}
       </section>
     </FriendsLayout>
   );
