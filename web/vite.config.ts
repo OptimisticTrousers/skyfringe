614984dbaa1924e7c1807ed77a2d21dc9bb75cea
OptimisticTrousers <tony.isern1@gmail.com>
2023-05-14 10:36:40 -0400

    Installing package for vitest coverage

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -8,10 +8,13 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
+    coverage: {
+      provider: "c8",
+    },
   },
 });
