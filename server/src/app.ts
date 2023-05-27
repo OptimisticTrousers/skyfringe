ae86d1d910eb72d6b1da820919c5f1d8946076e3
OptimisticTrousers <tony.isern1@gmail.com>
2023-05-26 23:15:20 -0400

    Adding CORS for production for server

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,13 +7,24 @@ import routes from "./routes/index";
 import passportConfig from "./config/passport";
 import notFoundHandler from "./middleware/notFoundHandler";
 import errorHandler from "./middleware/errorHandler";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.FRONTEND_URL
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 passportConfig();
 
