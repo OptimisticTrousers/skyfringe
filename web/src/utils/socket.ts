31e225b225a0471c98aced798e53239a24dc3998
OptimisticTrousers <tony.isern1@gmail.com>
2023-05-26 23:48:06 -0400

    Adding correct socket

--- web/src/utils/socket.ts
+++ web/src/utils/socket.ts
@@ -1,9 +1,6 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  import.meta.env.NODE_ENV === "production"
-    ? import.meta.env.VITE_API_DOMAIN
-    : "http://localhost:5000/api";
+const URL = import.meta.env.VITE_API_DOMAIN
 
 export const socket = io(URL);
