c4d9fdb91c63c001a87899277b9d1177e7902eba
OptimisticTrousers <tony.isern1@gmail.com>
2023-05-27 02:08:51 -0400

    Changing 'accessControlAllow'

--- server/src/middleware/accessControlAllow.ts
+++ server/src/middleware/accessControlAllow.ts
@@ -1,14 +1,27 @@
 import { Request, NextFunction, Response } from "express";
+import { config } from "dotenv";
+
+config();
+
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+if (!FRONTEND_URL) {
+  throw new Error("FRONTEND_URL value is not defined in .env file");
+}
 
 const accessControlAllow = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", FRONTEND_URL);
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
