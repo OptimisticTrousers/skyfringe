a6ae5f8eda1a9aa8ede2e783c1b861a8ac409fe5
OptimisticTrousers <tony.isern1@gmail.com>
2023-05-26 23:29:12 -0400

    Adding correct url for socket in production for web app

--- web/src/utils/socket.ts
+++ web/src/utils/socket.ts
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://localhost:5000";
+const URL =
+  import.meta.env.NODE_ENV === "production"
+    ? import.meta.env.VITE_API_DOMAIN
+    : "http://localhost:5000/api";
 
 export const socket = io(URL);
