7eb0b095a6156008864459ff35ab9ead076f8ba3
OptimisticTrousers <tony.isern1@gmail.com>
2023-05-27 00:35:07 -0400

    Fixing bug with facebook passport config

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -93,7 +93,7 @@ const passportConfig = () => {
           // Create a new User doc with their FB details
           const newUser = new User({
             facebookId: profile.id,
-            fullName: profile.name,
+            fullName: profile.name.givenName + " " + profile.name.familyName,
             userName: profile.name.givenName.toLowerCase(),
             email: profile.emails[0].value,
             // Image will always be available. Is set to anon img if FB profile has not changed. No alt text available for these unfortunately
