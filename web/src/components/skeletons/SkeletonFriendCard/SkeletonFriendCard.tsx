be56e45de94f564f2e59e8aa8688985ff6b0547a
OptimisticTrousers <tony.isern@yahoo.com>
2024-03-26 15:45:06 -0400

    Fixing color schemes

--- web/src/components/skeletons/SkeletonFriendCard/SkeletonFriendCard.tsx
+++ web/src/components/skeletons/SkeletonFriendCard/SkeletonFriendCard.tsx
@@ -1,14 +1,17 @@
 import CSSModules from "react-css-modules";
 import styles from "./SkeletonFriendCard.module.css";
 import Shimmer from "../Shimmer/Shimmer";
+import { useContext } from "react";
+import { ThemeContext } from "../../../context/ThemeContext";
 
 const SkeletonFriendCard = () => {
+  const { theme } = useContext(ThemeContext);
   return (
     <div styleName="skeleton">
-      <div styleName="skeleton__avatar"></div>
+      <div styleName="skeleton__avatar" className={`skeleton skeleton--${theme}`}></div>
       <div styleName="skeleton__details">
-        <div styleName="skeleton__text"></div>
-        <div styleName="skeleton__button"></div>
+        <div styleName="skeleton__text" className={`skeleton skeleton--${theme}`}></div>
+        <div styleName="skeleton__button" className={`skeleton skeleton--${theme}`}></div>
       </div>
       <Shimmer />
     </div>
